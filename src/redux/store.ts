c4a3a8913ac572da3c6a093475283d6e3962ad30
adamhinton <adam.d.hinton@gmail.com>
2024-12-28 09:36:32 -0500

    basic redux config

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,6 @@
 // README
 // Per the Redux docs https://redux.js.org/usage/nextjs -- to be more compatible with Next, this creates and returns a new store for every request. This is to synthesize better with RSC etc.
+// NOTE that any component that interacts with the store must be a client component.
 
 import { configureStore } from "@reduxjs/toolkit";
 
@@ -9,8 +10,6 @@ export const makeStore = () => {
 	});
 };
 
-// Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore["getState"]>;
 export type AppDispatch = AppStore["dispatch"];
