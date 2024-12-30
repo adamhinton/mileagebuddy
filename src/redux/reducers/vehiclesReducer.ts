1d61047a112f649b12c95268dd6368e34e8d47f5
adamhinton <adam.d.hinton@gmail.com>
2024-12-30 09:09:22 -0500

    Fleshing out vehiclesReducer

--- src/redux/reducers/vehiclesReducer.ts
+++ src/redux/reducers/vehiclesReducer.ts
@@ -1,6 +1,9 @@
 // README:
 // This is the meat and potatoes of the whole project
 // User enters data about their vehicle, then we calculate how their cost per mile
+// Can have multiple vehicles
+
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 // All fields are required because they'll be 0 if they don't apply
 export type Vehicle = {
@@ -19,4 +22,29 @@ export type Vehicle = {
 	monthlyParkingCosts: number;
 	monthlyTolls: number;
 	monthlyCarWashes: number;
+	miscellaneousMonthlyCosts: number;
 };
+
+// vehicles reducer
+const vehiclesSlice = createSlice({
+	name: "vehicles",
+	initialState: [] as Vehicle[],
+	reducers: {
+		addVehicle: (state, action: PayloadAction<Vehicle>) => {
+			state.push(action.payload);
+		},
+		removeVehicle: (state, action: PayloadAction<number>) => {
+			state.splice(action.payload, 1);
+		},
+		setVehicles: (state, action: PayloadAction<Vehicle[]>) => {
+			state.length = 0;
+			state.push(...action.payload); 
+	},
+    // removeAllVehicles
+     removeAllVehicles: (state) => {
+         state.length = 0;
+      }
+});
+
+export const { addVehicle, removeOneVehicle, setVehicles, removeAllVehicles } = vehiclesSlice.actions;
+export default vehiclesSlice.reducer;
