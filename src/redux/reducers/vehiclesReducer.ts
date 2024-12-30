ad77934a7eb2eaeac4107914d0d0fc24c4507f4a
adamhinton <adam.d.hinton@gmail.com>
2024-12-30 09:01:46 -0500

    delete old comments

--- src/redux/reducers/vehiclesReducer.ts
+++ src/redux/reducers/vehiclesReducer.ts
@@ -2,23 +2,6 @@
 // This is the meat and potatoes of the whole project
 // User enters data about their vehicle, then we calculate how their cost per mile
 
-// Vehicle factors:
-// vehicleName
-// year
-// make
-// model
-// Gas cost per gallon
-// Miles per gallon
-// yearly maintenance cost (oil changes, tires, etc), approximated
-// yearly insurance cost
-// yearly registration cost
-// yearly taxes
-// yearly depreciation - this is tough to estimate. When do you expect to sell the car, and for how much?
-// Monthly payments
-// Monthly parking costs
-// Monthly tolls
-// Monthly car washes
-
 // All fields are required because they'll be 0 if they don't apply
 export type Vehicle = {
 	vehicleName: string;
