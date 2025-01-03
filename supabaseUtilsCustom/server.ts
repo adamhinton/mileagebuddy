3b66c0b24465827fcaaf4f8d7981796046ea6919
adamhinton <adam.d.hinton@gmail.com>
2025-01-03 08:56:24 -0500

    server.ts util to create server client for access to supabase db

---
+++ supabaseUtilsCustom/server.ts
@@ -0,0 +1,35 @@
+// README:
+// To access Supabase you need to create a "client" in the browser or server
+// The process is different in SSR components vs client components
+// This util creates a Client for your server component
+// You can make a new client in each file, that's totally fine
+
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createClient() {
+	const cookieStore = await cookies();
+
+	return createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(({ name, value, options }) =>
+							cookieStore.set(name, value, options)
+						);
+					} catch {
+						// The `setAll` method was called from a Server Component.
+						// This can be ignored if you have middleware refreshing
+						// user sessions.
+					}
+				},
+			},
+		}
+	);
+}
