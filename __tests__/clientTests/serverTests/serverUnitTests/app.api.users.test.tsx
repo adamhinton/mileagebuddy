2197b8241d6243fd136deeabc65b1c0425ca1a07
adamhinton <adam.d.hinton@gmail.com>
2025-01-04 13:57:53 -0500

    add jest-environment

--- __tests__/clientTests/serverTests/serverUnitTests/app.api.users.test.tsx
+++ __tests__/clientTests/serverTests/serverUnitTests/app.api.users.test.tsx
@@ -1,3 +1,9 @@
 // README
 // This file is for UNIT tests. It tests API endpoint logic with dummy data, doesn't interact with an actual DB.
 //  See README in servertests/serverunit tests for more info.
+
+// IMPORTANT: All backend tests must have this at the top of the file, because their jest-environment is different from the one specified in our jest config (since it's different for client vs server tests)
+
+/**
+ * @jest-environment node
+ */
