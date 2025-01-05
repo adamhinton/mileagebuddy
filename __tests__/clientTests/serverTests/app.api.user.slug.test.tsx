7eb4f1eee24f6c63488ef2313ef5b5b5ed9fefd0
adamhinton <adam.d.hinton@gmail.com>
2025-01-04 23:20:52 -0500

    beginning of app.api.user.slug.test

---
+++ __tests__/clientTests/serverTests/app.api.user.slug.test.tsx
@@ -0,0 +1,11 @@
+// README
+// this is, obviously, a test file for the server-side API route /api/user/slug
+// testing CRUD operations for a user by id
+
+// This file is for UNIT tests. It tests API endpoint logic with dummy data, doesn't interact with an actual DB.
+//  See README in servertests/serverunit tests for more info.
+
+// IMPORTANT: All backend tests must have this jest-environment flag at the top of the file, because their jest-environment is different from the one specified in our jest config (since it's different for client vs server tests)
+/**
+ * @jest-environment node
+ */
