40815122f73b42c3ac9da9ebfdea72ec19f2d17a
adamhinton <adam.d.hinton@gmail.com>
2025-01-05 16:20:44 -0500

    Code cleanup

--- src/app/api/user/[slug]/route.ts
+++ src/app/api/user/[slug]/route.ts
@@ -40,6 +40,11 @@ export async function DELETE(
 
 	const supabase = await createClientSSROnly();
 
+	// const isUserExistsInDB = await checkIfUserExistsInDB(slug);
+	// if (!isUserExistsInDB) {
+	// 	return NextResponse.json({ message: "User not in DB" }, { status: 404 });
+	// }
+
 	const { data, error } = (await supabase
 		.from("users")
 		.delete()
@@ -64,80 +69,7 @@ export async function DELETE(
 	return NextResponse.json({ message: "User deleted successfully" });
 }
 
-// update user
-// put without `any` type, without 	`request.json()` (that function doesn't exist), with error handling for general supabase error and user not found error
-// Make sure to update any user field, not just id. But id is required so we can look up user. This looks up user by id
-// export async function PUT(
-// 	request: Request,
-// 	{ params }: { params: Promise<{ slug: string }> }
-// ) {
-// 	console.log("PUT running");
-// 	const slug = (await params).slug;
-// 	console.log("PUT request.body:", request.body);
-
-// 	const supabase = await createClientSSROnly();
-
-// 	const { data, error } = (await supabase
-// 		.from("users")
-// 		.update(request.body)
-// 		.eq("id", slug)) as {
-// 		data: object[] | null;
-// 		error: { message: string } | null;
-// 	};
-
-// 	if (error) {
-// 		return NextResponse.json({ error: error.message }, { status: 500 });
-// 	}
-
-// 	console.log("error in PUT:", error);
-// 	console.log("data in PUT:", data);
-
-// 	if (!data || data.length === 0) {
-// 		return NextResponse.json({ error: "User not found" }, { status: 404 });
-// 	}
-
-// 	return NextResponse.json(data);
-// }
-
-// This version successfully updates user but still returns an error to frontend
-// export async function PUT(
-// 	request: Request,
-// 	{ params }: { params: Promise<{ slug: string }> }
-// ) {
-// 	const { slug } = await params; // Await to get the slug properly
-
-// 	try {
-// 		const body = await request.json();
-
-// 		const supabase = await createClientSSROnly();
-
-// 		const { data, error } = (await supabase
-// 			.from("users")
-// 			.update(body)
-// 			.eq("id", slug)) as {
-// 			data: object[] | null;
-// 			error: { message: string } | null;
-// 		};
-
-// 		console.log("data in PUT:", data);
-// 		console.log("error in PUT:", error);
-
-// 		if (error) {
-// 			return NextResponse.json({ error: error.message }, { status: 500 });
-// 		}
-
-// 		if (!data || data.length === 0) {
-// 			console.log("PUT !data || data.length === 0");
-// 			return NextResponse.json({ error: "User not found" }, { status: 404 });
-// 		}
-
-// 		return NextResponse.json(data);
-// 	} catch (error) {
-// 		console.log("catch path");
-// 		return NextResponse.json({ error: error }, { status: 500 });
-// 	}
-// }
-
+// Can update this to take in more than just id if needed
 export async function PUT(
 	request: Request,
 	{ params }: { params: Promise<{ slug: string }> }
@@ -154,8 +86,6 @@ export async function PUT(
 			return NextResponse.json({ message: "User not in DB" }, { status: 404 });
 		}
 
-		// check if user exists first and return 404 not found if not
-
 		// Explicitly type the response from Supabase
 		const {
 			data,
@@ -164,9 +94,6 @@ export async function PUT(
 			.from("users")
 			.update(body)
 			.eq("id", slug);
-
-		console.log("data in PUT:", data);
-		console.log("error in PUT:", error);
 
 		// error from supabase
 		if (error) {
@@ -184,7 +111,6 @@ export async function PUT(
 			return NextResponse.json({ error: error }, { status: 500 });
 		} else {
 			// this should rarely occur
-			console.log("catch path: unknown error", error);
 			return NextResponse.json(
 				{ error: "Unknown error occurred" },
 				{ status: 500 }
