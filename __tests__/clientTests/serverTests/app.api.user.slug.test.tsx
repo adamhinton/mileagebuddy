5c32d6698ea7c7e7fad12e33fcbd4b87e4355dfe
adamhinton <adam.d.hinton@gmail.com>
2025-01-04 23:31:22 -0500

    basic sanity check test, will flesh out now

--- __tests__/clientTests/serverTests/app.api.user.slug.test.tsx
+++ __tests__/clientTests/serverTests/app.api.user.slug.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // README
 // this is, obviously, a test file for the server-side API route /api/user/slug
 // testing CRUD operations for a user by id
@@ -9,3 +10,30 @@
 /**
  * @jest-environment node
  */
+
+import { User } from "@/zod/schemas/UserSchema";
+
+// Mock the Supabase client
+jest.mock("../../../supabaseUtilsCustom/server", () => ({
+	createClientSSROnly: jest.fn(),
+}));
+
+// Mock NextResponse
+jest.mock("next/server", () => ({
+	NextResponse: {
+		json: jest.fn((data) => ({
+			json: jest.fn().mockResolvedValue(data), // This ensures the mock returns the passed data
+		})),
+	},
+}));
+
+// describe GET api/user/id. It should return a user by id
+// make a mock user object of type User
+
+describe("GET /api/user/slug", () => {});
+
+describe("sanity check", () => {
+	it("should return true", () => {
+		expect(true).toBe(true);
+	});
+});
