6f3fa4038c733d8939bdd0d4d1257042cec70092
adamhinton <adam.d.hinton@gmail.com>
2025-01-04 23:18:21 -0500

    All single user CRUD operations

--- src/app/api/user/[slug]/route.ts
+++ src/app/api/user/[slug]/route.ts
@@ -4,6 +4,8 @@
 import { NextResponse } from "next/server";
 import { createClientSSROnly } from "../../../../../supabaseUtilsCustom/server";
 
+// get user by id
+// Tested, works
 export async function GET(
 	request: Request,
 	{ params }: { params: Promise<{ slug: string }> }
@@ -20,6 +22,66 @@ export async function GET(
 		.select("*")
 		.eq("id", slug);
 	console.log("data in GET:", data);
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+
+	return NextResponse.json(data);
+}
+
+// delete user by id
+// Tested, works
+export async function DELETE(
+	request: Request,
+	{ params }: { params: Promise<{ slug: string }> }
+) {
+	const slug = (await params).slug;
+
+	const supabase = await createClientSSROnly();
+
+	const { data, error } = await supabase.from("users").delete().eq("id", slug);
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+
+	return NextResponse.json(data);
+}
+
+// update user
+// TODO: Test this
+export async function PUT(
+	request: Request,
+	{ params }: { params: Promise<{ slug: string }> }
+) {
+	const slug = (await params).slug;
+
+	const supabase = await createClientSSROnly();
+
+	const { data, error } = await supabase
+		.from("users")
+		.update({ username: "newUsername" })
+		.eq("id", slug);
+
+	if (error) {
+		return NextResponse.json({ error: error.message }, { status: 500 });
+	}
+
+	return NextResponse.json(data);
+}
+
+// create user
+// TODO: Test this
+export async function POST(
+	request: Request,
+	{ params }: { params: Promise<{ slug: string }> }
+) {
+	const slug = (await params).slug;
+
+	const supabase = await createClientSSROnly();
+
+	const { data, error } = await supabase.from("users").insert({ id: slug });
 
 	if (error) {
 		return NextResponse.json({ error: error.message }, { status: 500 });
