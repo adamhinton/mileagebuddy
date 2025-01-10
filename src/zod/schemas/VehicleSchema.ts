de3fcbb809caeb8342d57a174cd5b73a384fc2f6
adamhinton <adam.d.hinton@gmail.com>
2025-01-10 11:23:46 -0500

    WIP updating VehicleSchema

--- src/zod/schemas/VehicleSchema.ts
+++ src/zod/schemas/VehicleSchema.ts
@@ -1,22 +1,46 @@
 import { z } from "zod";
 
+// TODO: Account for electric
+
 export const VehicleSchema = z.object({
 	vehicleName: z.string(),
-	year: z.number(),
-	make: z.string(),
-	model: z.string(),
-	gasCostPerGallon: z.number(),
-	milesPerGallon: z.number(),
-	yearlyMaintenanceCost: z.number(),
-	yearlyInsuranceCost: z.number(),
-	yearlyRegistrationCost: z.number(),
-	yearlyTaxes: z.number(),
+	year: z.number().nullable(),
+	make: z.string().nullable(),
+	model: z.string().nullable(),
+	purchasePrice: z.number(),
+	typicalUse: z.object({
+		averageDailyMiles: z.number(),
+		weeksPerYear: z.number(),
+		percentHighway: z.number().min(0).max(100),
+	}),
+	additionalUse: z.object({
+		distanceMiles: z.number().nonnegative(),
+	}),
+	milesPerGallon: z.object({
+		highwayMPG: z.number().positive(),
+		cityMPG: z.number().positive(),
+	}),
+	/**How many years until the car is sold */
+	willSellCarAfterYears: z.number(),
+	/** About what price will you sell the car at */
+	willSellCarAtPrice: z.number(),
+	fixedCosts: z.object({
+		yearlyInsuranceCost: z.number().nonnegative(),
+		yearlyRegistrationCost: z.number().nonnegative(),
+		yearlyTaxes: z.number().nonnegative(),
+		monthlyPayments: z.number().nonnegative(),
+		monthlyWarrantyCost: z.number().nonnegative(),
+	}),
+	yearlyMaintenanceCost: z.number().nonnegative(),
 	yearlyDepreciation: z.number(),
-	monthlyPayments: z.number(),
-	monthlyParkingCosts: z.number(),
-	monthlyTolls: z.number(),
-	monthlyCarWashes: z.number(),
-	miscellaneousMonthlyCosts: z.number(),
+	monthlyParkingCosts: z.number().nonnegative(),
+	monthlyTolls: z.number().nonnegative(),
+	monthlyCarWashCost: z.number().nonnegative(),
+	monthlyMiscellaneousCosts: z.number().nonnegative(),
+	downPaymentAmount: z.number().nonnegative(),
+	// trim: z.string(),
+	/**Anything you get reimbursed for etc. These reduce your costs */
+	monthlyCostDeductions: z.number().nonnegative(),
 });
 
 export type Vehicle = z.infer<typeof VehicleSchema>;
