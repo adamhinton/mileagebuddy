4095c739ff313dd0c9fb6c2a33606f3e1924b630
adamhinton <adam.d.hinton@gmail.com>
2025-01-11 17:34:55 -0500

    About to start on getVehiclesById

--- src/app/api/vehicles/route.ts
+++ src/app/api/vehicles/route.ts
@@ -1,14 +1,12 @@
 // import { NextResponse } from "next/server";
 import { NextResponse } from "next/server";
 import { createClientSSROnly } from "../../../../supabaseUtilsCustom/server";
+import { SupabaseClient } from "@supabase/supabase-js";
 
 const vehiclesTableName = "vehicles";
 
 // TODO: Make sure this only returns data for authenticated User id
 export async function GET(request: Request) {
-	// return NextResponse.json("Hello world vehicles", { status: 200 });
-	// get all vehicles from vehicles table where userid= params id (api/vehicles?userid=1)
-
 	const url = new URL(request.url!);
 
 	type PartialVehicle = {
@@ -17,28 +15,36 @@ export async function GET(request: Request) {
 		type: "gas" | "electric";
 	};
 
+	type VehiclesDataResponse = {
+		error: string;
+		data: PartialVehicle[];
+	};
+
 	try {
 		const supabase = await createClientSSROnly();
 
 		const userID = url.searchParams.get("userid");
 
 		/**If this is null, get all that user's vehicles */
-		const vehicleID = url.searchParams.get("vehicleID");
+		const vehicleID = url.searchParams.get("vehicleid");
 
-		let vehiclesData: PartialVehicle[];
+		let vehiclesData: {
+			error: string;
+			data: PartialVehicle[];
+		};
 		if (vehicleID) {
 			vehiclesData = (await supabase
 				.from(vehiclesTableName)
 				.select("id, userid, type")
 				.eq("id", vehicleID)
 				// TODO: Better typing here
-				.single()) as unknown as PartialVehicle[];
+				.single()) as unknown as VehiclesDataResponse;
 		} else {
 			vehiclesData = (await supabase
 				.from(vehiclesTableName)
-				.select("id, userid, type, createdat, updatedat")
+				.select("id, userid, type")
 				// TODO: Better typing here
-				.eq("userid", userID)) as unknown as PartialVehicle[];
+				.eq("userid", userID)) as unknown as VehiclesDataResponse;
 		}
 
 		if (!vehiclesData) {
@@ -52,57 +58,62 @@ export async function GET(request: Request) {
 
 		console.log("vehiclesData:", vehiclesData);
 
-		const vehicleIDs = Array.isArray(vehiclesData)
-			? vehiclesData.map((v) => v.id)
+		const vehicleIDs = Array.isArray(vehiclesData.data)
+			? vehiclesData.data.map((v) => v.id)
 			: [vehicleID];
 
+		console.log("vehicleIDs:", vehicleIDs);
+
+		// There are multiple different tables containing different kinds of user data. This pulls them all together.
 		const vehicleInfoPromises = vehicleIDs.map((id) =>
 			Promise.all([
 				supabase
-					.from("vehicleData")
-					.select("vehicleName, year, make, model, trim, highwayMPG")
+					.from("vehicledata")
+					.select("vehiclename, year, make, model, trim, highwaympg")
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("gasVehicleData")
-					.select("gasCostPerGallon, milesPerGallonHighway, milesPerGallonCity")
+					// Will return error/null if vehicle is electric. That's normal and expected
+					.from("gasvehicledata")
+					.select("gascostpergallon, milespergallonhighway, milespergalloncity")
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("electricVehicleData")
-					.select("costPerCharge, milesPerCharge, electricRangeMiles")
+					// Will return error/null if vehicle is gas. That's normal and expected
+					.from("electricvehicledata")
+					.select("costpercharge, milespercharge, electricrangemiles")
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("purchaseAndSales")
+					.from("purchaseandsales")
 					.select(
-						"yearPurchased, purchasePrice, downPaymentAmount, willSellCarAfterYears, milesBoughtAt, willSellCarAtMiles, willSellCarAtPrice"
+						"yearpurchased, purchaseprice, downpaymentamount, willsellcarafteryears, milesboughtat, willsellcaratmiles, willsellcaratprice"
 					)
 					.eq("vehicleid", id)
 					.single(),
 				supabase
 					.from("usage")
 					.select(
-						"averageDailyMiles, weeksPerYear, percentHighway, extraDistanceMiles, extraDistancePercentHighway"
+						"averagedailymiles, weeksperyear, percenthighway, extradistancemiles, extradistancepercenthighway"
 					)
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("fixedCosts")
+					.from("fixedcosts")
 					.select(
-						"yearlyInsuranceCost, yearlyRegistrationCost, yearlyTaxes, monthlyLoanPayment, monthlyWarrantyCost, inspectionCost, otherYearlyCosts"
+						"yearlyinsurancecost, yearlyregistrationcost, yearlytaxes, monthlyloanpayment, monthlywarrantycost, inspectioncost, otheryearlycosts"
 					)
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("yearlyMaintenanceCosts")
-					.select("oilChanges, tires, batteries, brakes, other, depreciation")
+					.from("yearlymaintenancecosts")
+					.select("oilchanges, tires, batteries, brakes, other, depreciation")
 					.eq("vehicleid", id)
 					.single(),
 				supabase
-					.from("variableCosts")
+					.from("variablecosts")
 					.select(
-						"monthlyParkingCosts, monthlyTolls, monthlyCarWashCost, monthlyMiscellaneousCosts, monthlyCostDeductions"
+						"monthlyparkingcosts, monthlytolls, monthlycarwashcost, monthlymiscellaneouscosts, monthlycostdeductions"
 					)
 					.eq("vehicleid", id)
 					.single(),
@@ -112,17 +123,31 @@ export async function GET(request: Request) {
 		const vehicleInfos = await Promise.all(vehicleInfoPromises);
 
 		const fullVehiclesInfo = vehicleInfos.map((info, index) => {
-			const vehicleData = vehicleID ? vehiclesData : vehiclesData[index];
+			const vehicleData = vehicleID ? vehiclesData : vehiclesData.data[index];
+
+			const basicVehicleData = info[0].data;
+			const gasVehicleData = info[1].data;
+			const electricVehicleData = info[2].data;
+			const purchaseAndSalesData = info[3].data;
+			const usageData = info[4].data;
+			const fixedCostsData = info[5].data;
+			const yearlyMaintenanceCostsData = info[6].data;
+			const variableCosts = info[7].data;
+
+			console.log("info:", info);
+
 			return {
+				// TODO: Name these for easier reading
 				...vehicleData,
-				...info[0],
-				...info[1],
-				...info[2],
-				...info[3],
-				...info[4],
-				...info[5],
-				...info[6],
-				...info[7],
+				...basicVehicleData,
+				// either gas vehicle info or electric vehicle info
+				// both can't exist together
+				...(gasVehicleData ? gasVehicleData : electricVehicleData),
+				...purchaseAndSalesData,
+				...usageData,
+				...fixedCostsData,
+				...yearlyMaintenanceCostsData,
+				...variableCosts,
 			};
 		});
 
@@ -137,3 +162,8 @@ export async function GET(request: Request) {
 		);
 	}
 }
+
+const getVehiclesById = async (
+	vehicleIDs: string[],
+	supabase: SupabaseClient
+) => {};
