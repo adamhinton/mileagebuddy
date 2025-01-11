ffc8bd8359c84511c0ad1f67219ec25c6cc965b0
adamhinton <adam.d.hinton@gmail.com>
2025-01-11 13:43:46 -0500

    add Todo items

--- supabase/migrations/20250101024117_create_user_table.sql
+++ supabase/migrations/20250101024117_create_user_table.sql
@@ -1,5 +1,12 @@
 -- Note that in db calls isdarkmode has to be all lower case for postgres reasons
 
+-- TODO:
+-- ADD TO TABLE:
+-- createdAT
+-- updatedAt
+-- deletedAt
+-- And update this in our CRUD endpoints
+
 CREATE TABLE users (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   email text UNIQUE NOT NULL,
