abb269b4ed9790e0a3fd6af8cf43135f9b1a9c00
adamhinton <adam.d.hinton@gmail.com>
2025-01-14 09:46:32 -0500

    Create new db function to INSERT user

---
+++ supabase/migrations/20250114142437_insert_vehicle_function.sql
@@ -0,0 +1,97 @@
+-- README
+-- Vehicles are complex objects, so we have multiple sub-tables storing data about vehicles.
+-- See GetVehicleTypes.ts for more info about the type
+-- This is a function to add a new Vehicle, inserting all its relevant values to the different tables
+-- It only works if all inserts are successful; if not it rolls back all changes and throws error
+
+CREATE OR REPLACE FUNCTION insert_vehicle(
+    _userID bigint,
+    _type VARCHAR(10),
+    _vehiclesOrder INT,
+    _vehicleData jsonb,
+    _gasVehicleData jsonb,  -- Optional, depending on the vehicle type
+    _electricVehicleData jsonb, -- Optional, depending on the vehicle type
+    _purchaseAndSales jsonb,
+    _usage jsonb,
+    _fixedCosts jsonb,
+    _yearlyMaintenanceCosts jsonb,
+    _variableCosts jsonb
+)
+RETURNS VOID AS $$
+DECLARE
+    new_vehicle_id INTEGER;
+BEGIN
+    -- Begin a transaction block
+    BEGIN
+        -- Insert into the vehicles table
+        INSERT INTO vehicles("userID", "type", "vehiclesOrder")
+        VALUES (_userID, _type, _vehiclesOrder)
+        RETURNING id INTO new_vehicle_id;
+
+        -- Insert vehicleData (can be applied for all vehicles)
+        INSERT INTO "vehicleData" ("vehicleID", "vehicleName", "year", "make", "model", "trim", "highwayMPG")
+        VALUES (new_vehicle_id, _vehicleData->>'vehicleName', (_vehicleData->>'year')::INT, _vehicleData->>'make', 
+                _vehicleData->>'model', _vehicleData->>'trim', (_vehicleData->>'highwayMPG')::DECIMAL);
+
+        -- Insert into gasVehicleData only for 'gas' type vehicles
+        IF _type = 'gas' THEN
+            INSERT INTO "gasVehicleData" ("vehicleID", "gasCostPerGallon", "milesPerGallonHighway", "milesPerGallonCity")
+            VALUES (new_vehicle_id, (_gasVehicleData->>'gasCostPerGallon')::DECIMAL, 
+                    (_gasVehicleData->>'milesPerGallonHighway')::DECIMAL,
+                    (_gasVehicleData->>'milesPerGallonCity')::DECIMAL);
+        END IF;
+
+        -- Insert into electricVehicleData only for 'electric' type vehicles
+        IF _type = 'electric' THEN
+            INSERT INTO "electricVehicleData" ("vehicleID", "costPerCharge", "milesPerCharge", "electricRangeMiles")
+            VALUES (new_vehicle_id, (_electricVehicleData->>'costPerCharge')::DECIMAL, 
+                    (_electricVehicleData->>'milesPerCharge')::DECIMAL,
+                    (_electricVehicleData->>'electricRangeMiles')::DECIMAL);
+        END IF;
+
+        -- Insert into purchaseAndSales table
+        INSERT INTO "purchaseAndSales" ("vehicleID", "yearPurchased", "purchasePrice", "downPaymentAmount", 
+                                        "willSellCarAfterYears", "milesBoughtAt", "willSellCarAtMiles", 
+                                        "willSellCarAtPrice")
+        VALUES (new_vehicle_id, (_purchaseAndSales->>'yearPurchased')::INT, (_purchaseAndSales->>'purchasePrice')::DECIMAL,
+                (_purchaseAndSales->>'downPaymentAmount')::DECIMAL, (_purchaseAndSales->>'willSellCarAfterYears')::INT,
+                (_purchaseAndSales->>'milesBoughtAt')::DECIMAL, (_purchaseAndSales->>'willSellCarAtMiles')::DECIMAL,
+                (_purchaseAndSales->>'willSellCarAtPrice')::DECIMAL);
+
+        -- Insert into usage table
+        INSERT INTO usage ("vehicleID", "averageDailyMiles", "weeksPerYear", "percentHighway", 
+                           "extraDistanceMiles", "extraDistancePercentHighway")
+        VALUES (new_vehicle_id, (_usage->>'averageDailyMiles')::DECIMAL, (_usage->>'weeksPerYear')::INT,
+                (_usage->>'percentHighway')::DECIMAL, (_usage->>'extraDistanceMiles')::DECIMAL,
+                (_usage->>'extraDistancePercentHighway')::DECIMAL);
+
+        -- Insert into fixedCosts table
+        INSERT INTO "fixedCosts" ("vehicleID", "yearlyInsuranceCost", "yearlyRegistrationCost", "yearlyTaxes", 
+                                  "yearlyParkingCost", "monthlyLoanPayment", "monthlyWarrantyCost", 
+                                  "inspectionCost", "otherYearlyCosts")
+        VALUES (new_vehicle_id, (_fixedCosts->>'yearlyInsuranceCost')::DECIMAL, (_fixedCosts->>'yearlyRegistrationCost')::DECIMAL, 
+                (_fixedCosts->>'yearlyTaxes')::INT, (_fixedCosts->>'yearlyParkingCost')::DECIMAL, 
+                (_fixedCosts->>'monthlyLoanPayment')::INT, (_fixedCosts->>'monthlyWarrantyCost')::INT,
+                (_fixedCosts->>'inspectionCost')::INT, (_fixedCosts->>'otherYearlyCosts')::INT);
+
+        -- Insert into yearlyMaintenanceCosts table
+        INSERT INTO "yearlyMaintenanceCosts" ("vehicleID", "oilChanges", "tires", "batteries", "brakes", "other", "depreciation")
+        VALUES (new_vehicle_id, (_yearlyMaintenanceCosts->>'oilChanges')::DECIMAL, (_yearlyMaintenanceCosts->>'tires')::DECIMAL,
+                (_yearlyMaintenanceCosts->>'batteries')::DECIMAL, (_yearlyMaintenanceCosts->>'brakes')::DECIMAL,
+                (_yearlyMaintenanceCosts->>'other')::DECIMAL, (_yearlyMaintenanceCosts->>'depreciation')::DECIMAL);
+
+        -- Insert into variableCosts table
+        INSERT INTO "variableCosts" ("vehicleID", "monthlyParkingCosts", "monthlyTolls", "monthlyCarWashCost", 
+                                     "monthlyMiscellaneousCosts", "monthlyCostDeductions")
+        VALUES (new_vehicle_id, (_variableCosts->>'monthlyParkingCosts')::DECIMAL, (_variableCosts->>'monthlyTolls')::DECIMAL,
+                (_variableCosts->>'monthlyCarWashCost')::DECIMAL, (_variableCosts->>'monthlyMiscellaneousCosts')::DECIMAL,
+                (_variableCosts->>'monthlyCostDeductions')::DECIMAL);
+
+        -- Commit the transaction (this is implicit since we're using a transaction block)
+    EXCEPTION
+        WHEN OTHERS THEN
+            -- If any part fails, we roll back the transaction
+            RAISE EXCEPTION 'Error inserting vehicle and its data: %', SQLERRM;
+    END;
+END;
+$$ LANGUAGE plpgsql;
