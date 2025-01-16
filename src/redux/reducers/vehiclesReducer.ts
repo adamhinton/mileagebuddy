2d333aa028d0a18123dac4a161ed099f125da881
adamhinton <adam.d.hinton@gmail.com>
2025-01-15 20:15:12 -0500

    Better typing in vehiclesReducer

--- src/redux/reducers/vehiclesReducer.ts
+++ src/redux/reducers/vehiclesReducer.ts
@@ -4,11 +4,9 @@
 // Can have multiple vehicles
 
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { Vehicle } from "@/zod/schemas/VehicleSchema";
+import { Vehicles, Vehicle } from "@/utils/server/types/GetVehicleTypes";
 
-type VehiclesState = Vehicle[];
-
-const initialState: VehiclesState = [];
+const initialState: Vehicles = mockVehicles;
 
 // vehicles reducer
 const vehiclesSlice = createSlice({
@@ -35,21 +33,72 @@ export const { addVehicle, removeOneVehicle, setVehicles, removeAllVehicles } =
 	vehiclesSlice.actions;
 export default vehiclesSlice.reducer;
 
-export const dummyVehicle: Vehicle = {
-	vehicleName: "My Car",
-	year: 2021,
-	make: "Toyota",
-	model: "Corolla",
-	gasCostPerGallon: 3.0,
-	milesPerGallon: 30,
-	yearlyMaintenanceCost: 1000,
-	yearlyInsuranceCost: 1000,
-	yearlyRegistrationCost: 100,
-	yearlyTaxes: 100,
-	yearlyDepreciation: 5000,
-	monthlyPayments: 0,
-	monthlyParkingCosts: 0,
-	monthlyTolls: 0,
-	monthlyCarWashes: 0,
-	miscellaneousMonthlyCosts: 0,
-};
+const mockVehicles: Vehicles = [
+	{
+		type: "gas",
+		userid: 1,
+		vehiclesOrder: 1,
+		id: 1,
+		vehicleData: {
+			vehicleName: "Tesla Model 3",
+			vehicleID: 1,
+			year: 2020,
+			make: "Tesla",
+			model: "Model 3",
+			trim: "Base",
+			highwayMPG: 35.5,
+		},
+		gasVehicleData: {
+			vehicleID: 1,
+			gasCostPerGallon: 3.5,
+			milesPerGallonHighway: 35.5,
+			milesPerGallonCity: 35.5,
+		},
+		purchaseAndSales: {
+			vehicleID: 1,
+			yearPurchased: 2020,
+			purchasePrice: 22000.0,
+			downPaymentAmount: 2000.0,
+			willSellCarAfterYears: 5,
+			milesBoughtAt: 10000,
+			willSellCarAtMiles: 80000,
+			willSellCarAtPrice: 12000.0,
+		},
+		usage: {
+			vehicleID: 1,
+			averageDailyMiles: 100,
+			weeksPerYear: 52,
+			percentHighway: 0.5,
+			extraDistanceMiles: 0,
+			extraDistancePercentHighway: 0,
+		},
+		fixedCosts: {
+			vehicleID: 1,
+			yearlyInsuranceCost: 1000.0,
+			yearlyRegistrationCost: 100.0,
+			yearlyTaxes: 100.0,
+			monthlyLoanPayment: 300.0,
+			monthlyWarrantyCost: 30.0,
+			inspectionCost: 100.0,
+			otherYearlyCosts: 300.0,
+		},
+		yearlyMaintenanceCosts: {
+			vehicleID: 1,
+			oilChanges: 100.0,
+			tires: 200.0,
+			batteries: 300.0,
+			brakes: 100.0,
+			other: 100.0,
+			depreciation: 800.0,
+		},
+		variableCosts: {
+			vehicleID: 1,
+			monthlyParkingCosts: 100.0,
+			monthlyTolls: 50.0,
+			monthlyCarWashCost: 20.0,
+			monthlyMiscellaneousCosts: 50.0,
+			monthlyCostDeductions: 80.0,
+		},
+		electricVehicleData: null,
+	},
+];
