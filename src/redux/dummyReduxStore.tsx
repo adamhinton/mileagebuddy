6bba531a7088c093fe4c806c7bb351c5ec9804a4
adamhinton <adam.d.hinton@gmail.com>
2025-01-27 01:14:16 -0500

    Fix out of date imports

--- src/redux/dummyReduxStore.tsx
+++ src/redux/dummyReduxStore.tsx
@@ -3,12 +3,12 @@
 import React, { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { User } from "@/zod/schemas/UserSchema";
-import { Vehicle } from "@/utils/server/types/GetVehicleTypes";
 import userReducer from "./reducers/userReducer";
 import vehiclesReducer from "./reducers/vehiclesReducer";
 import isDarkModeReducer from "@/redux/reducers/darkModeReducer";
 import { RootState } from "@/redux/store";
+import { User } from "@/app/zod/schemas/UserSchema";
+import { Vehicle } from "@/app/utils/server/types/VehicleTypes/GetVehicleTypes";
 
 const fakeUser: User = {
 	id: "1",
