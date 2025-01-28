0541d177284ba4d3f0f5cb9b4427c310de6155b7
adamhinton <adam.d.hinton@gmail.com>
2025-01-28 02:58:30 -0500

    Skeleton calculateCarCostMain.ts

---
+++ src/app/utils/CarCostAlgorithm/calculateCarCostMain.ts
@@ -0,0 +1,14 @@
+// README
+// This is the main file for the algorithm that calculates the true cost of owning a car
+// It takes in an object of type Vehicle and performs various calculations to estimate the true cost per mile driven
+// This is currently (1.28.25) a WIP and will develop as the project evolves.
+
+// PLAN:
+// TEST EXTENSIVELY
+// Write sub-functions for each part of the calculation
+// TEST EXTENSIVELY
+// Write tests for each sub-function
+// TEST EXTENSIVELY
+// Did I mention TEST EXTENSIVELY?
+// Really, I'll regret it if I don't have thorough unit tests.
+// TEST EXTENSIVELY
