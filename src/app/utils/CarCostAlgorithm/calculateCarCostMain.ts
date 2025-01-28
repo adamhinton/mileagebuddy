b3099b53f3746387f54d4bfb170743dda0859c20
adamhinton <adam.d.hinton@gmail.com>
2025-01-28 03:04:14 -0500

    skeleton calculateCarCostMain function

--- src/app/utils/CarCostAlgorithm/calculateCarCostMain.ts
+++ src/app/utils/CarCostAlgorithm/calculateCarCostMain.ts
@@ -1,9 +1,16 @@
+"use server";
+
+import { Vehicle } from "../server/types/VehicleTypes/GetVehicleTypes";
+
 // README
 // This is the main file for the algorithm that calculates the true cost of owning a car
+// The primary function is exported from here
 // It takes in an object of type Vehicle and performs various calculations to estimate the true cost per mile driven
 // This is currently (1.28.25) a WIP and will develop as the project evolves.
 
 // PLAN:
+// "use server" at the top of each file
+// Make CarCostResults type. With zod schema?
 // TEST EXTENSIVELY
 // Write sub-functions for each part of the calculation
 // TEST EXTENSIVELY
@@ -12,3 +19,7 @@
 // Did I mention TEST EXTENSIVELY?
 // Really, I'll regret it if I don't have thorough unit tests.
 // TEST EXTENSIVELY
+
+export const calculateCarCostMain = (vehicle: Vehicle) => {
+	vehicle.id;
+};
