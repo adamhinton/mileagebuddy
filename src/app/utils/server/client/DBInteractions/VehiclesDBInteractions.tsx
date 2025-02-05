fd1320face485f17ac511e0d58b8a0d97471e20e
adamhinton <adam.d.hinton@gmail.com>
2025-02-05 12:36:36 -0600

    Fix issue where db interactions weren't working in prod; just moved them to the client since they don't expose sensitive data

--- src/app/utils/server/client/DBInteractions/VehiclesDBInteractions.tsx
+++ src/app/utils/server/client/DBInteractions/VehiclesDBInteractions.tsx
@@ -5,6 +5,7 @@
 // You call this from your CLIENT components and it hits the API endpoints for you
 // This code runs on the server so as not to expose our endpoints to the client
 // TODO: Implement validation on client before sending to server
+// I pass in the site's url (AKA process.env.VERCEL_URL) because I had trouble getting this URL on the server; TODO: Figure out how to get site URL on the server
 
 import { Vehicle, Vehicles } from "../../types/VehicleTypes/GetVehicleTypes";
 import { Vehicle_For_db_PATCH } from "../../types/VehicleTypes/PATCHVehicleTypes";
@@ -12,18 +13,31 @@ import { Vehicle_For_db_POST } from "../../types/VehicleTypes/POSTVehicleTypes";
 
 // TODO: Validate Vehicles on frontend
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+const baseUrl = new URL(
+	process.env.NEXT_PUBLIC_VERCEL_URL
+		? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+		: "http://localhost:3000"
+).href;
+console.log("baseUrl:", baseUrl);
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+console.log("API_BASE_URL:", API_BASE_URL);
 
 /** See api/vehicles/route.ts GET for the associated endpoint */
 export const getVehiclesByUserIDClient = async (
 	userID: string
+	// baseUrl: string
 ): Promise<Vehicles> => {
 	try {
 		console.log("blah blah blah");
-		const res = await fetch(`${baseUrl}/api/vehicles?userid=${userID}`, {
+		const res = await fetch(`${baseUrl}api/vehicles?userid=${userID}`, {
 			method: "GET",
+			headers: { accept: "application/json" },
 		});
+		console.log("res:", res);
 		const data: Vehicles = await res.json();
+		console.log("data:", data);
 
 		return data;
 	} catch (error) {
@@ -39,6 +53,7 @@ export const getVehiclesByUserIDClient = async (
 export const getSingleVehicleByIDClient = async (
 	vehicleID: number,
 	userID: number
+	// baseUrl: string
 ): Promise<[Vehicle?]> => {
 	try {
 		const res = await fetch(
@@ -60,6 +75,7 @@ export const getSingleVehicleByIDClient = async (
 export const insertVehicleClient = async (
 	// Don't need to know userid because Vehicle_For_db_POST has user ID
 	vehicle: Vehicle_For_db_POST
+	// baseUrl: string
 ): Promise<Vehicle> => {
 	try {
 		const res = await fetch(`${baseUrl}/api/vehicles`, {
@@ -86,6 +102,7 @@ export const insertVehicleClient = async (
  */
 export const deleteVehicleByIDClient = async (
 	vehicleID: number
+	// baseUrl: string
 ): Promise<Vehicle> => {
 	try {
 		const res = await fetch(`${baseUrl}/api/vehicles?vehicleid=${vehicleID}`, {
@@ -109,6 +126,7 @@ export const deleteVehicleByIDClient = async (
  */
 export const updateVehicleInDBClient = async (
 	vehicle: Vehicle_For_db_PATCH
+	// baseUrl: string
 ): Promise<Vehicle> => {
 	try {
 		const res = await fetch(`${baseUrl}/api/vehicles?vehicleid=${vehicle.id}`, {
