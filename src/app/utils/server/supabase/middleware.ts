1ff2058f4de39aa1dd26c2a040e4ddcbb199da10
adamhinton <adam.d.hinton@gmail.com>
2025-02-08 13:05:30 -0600

    utils/supabase/middleware.ts skeleton file

---
+++ src/app/utils/server/supabase/middleware.ts
@@ -0,0 +1,4 @@
+// README:
+// This is middleware specific to Supabase, per Supabase's docs
+// https://supabase.com/docs/guides/auth/server-side/nextjs
+// Note that this isn't the main middleware for the app, that's in middleware.ts at the root of the project
