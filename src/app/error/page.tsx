19ae282311f73bd759b69638d49e339dbc2d99fc
adamhinton <adam.d.hinton@gmail.com>
2025-02-08 13:18:17 -0600

    Frontend page for login and associated actions

---
+++ src/app/error/page.tsx
@@ -0,0 +1,10 @@
+// README:
+// I was following the supabase auth docs and it said to put this page here for testing.
+// supabase.com/docs/guides/auth/server-side/nextjs
+// TODO Delete this if it's no longer needed or doesn't fit the flow of the app.
+
+"use client";
+
+export default function ErrorPage() {
+	return <p>Sorry, something went wrong</p>;
+}
