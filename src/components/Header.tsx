905b1ccd8018acb8536a1ff95cb61f65572c2029
adamhinton <adam.d.hinton@gmail.com>
2025-02-13 12:17:30 -0500

    logout button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,13 +7,34 @@
 
 import React from "react";
 import ThemeSwitch from "./ThemeSwitch";
+import { createClientCSROnly } from "@/app/utils/server/supabase/client";
+import { useAppSelector } from "@/redux/hooks";
 
 export const Header: React.FC = () => {
+	const user = useAppSelector((state) => state.user.value);
+	console.log("redux user in Header:", user);
+
 	return (
 		<header className="bg-white dark:bg-slate-800 p-4 flex justify-between items-center">
 			<h1 className="text-2xl bg-white text-black dark:bg-black dark:text-white ">
 				Trip Planner
 			</h1>
+			{/* Just for testing */}
+			<h2>{user ? "Logged in" : "Not logged in "}</h2>
+			<button
+				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+				onClick={async () => {
+					const supabase = createClientCSROnly();
+					const { error } = await supabase.auth.signOut();
+					if (!error) {
+						console.log("Logged out");
+					} else {
+						console.error("Error logging out:", error.message);
+					}
+				}}
+			>
+				Sign Out
+			</button>
 			<ThemeSwitch />
 		</header>
 	);
