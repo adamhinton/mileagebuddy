d0c47675cf66c195cdc484fd433a6209449b4e00
adamhinton <adam.d.hinton@gmail.com>
2025-02-14 14:31:00 -0500

    Test was failing because it couldn't get env variables; configured Jest to get env variables

---
+++ jest.setup.ts
@@ -0,0 +1,6 @@
+// Are these causing tests to fail?
+// You need to manually change this if the values in env.local change
+
+process.env.NEXT_PUBLIC_SUPABASE_URL = "http://127.0.0.1:54321";
+process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY =
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxbmh6d2dheXB5d3ltaHFmYmdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU2NTYxNTEsImV4cCI6MjA1MTIzMjE1MX0.yazqZFTKbxkirlw2fd40InMShrUZ6utR2ESaLMJpnY0";
