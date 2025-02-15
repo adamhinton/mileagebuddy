24a0364c674f692ddae4c8a19a291be5b80903b0
adamhinton <adam.d.hinton@gmail.com>
2025-02-14 19:12:26 -0500

    Auth planning notes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,25 @@
 
 "use client";
 
+// AUTH TODO:
+
+// Users table:
+// Delete old users table
+// Hook up any relevant endpoints to new users table
+
+// Vehicles table TODO:
+// Hook up new user id to vehicles
+// Nothing needs done in vehicle sub-objects
+
+// Auth UI TODO:
+// Update flow:
+// Flow when loading page and already logged in
+// Flow when loading page not logged in
+// Where to put signin/ log out buttons etc
+
+// State/Authwatcher TODO (related to Auth UI todo):
+// Figure out how to call functions on signup
+
 import { useState, useEffect } from "react";
 import { createClientCSROnly } from "../utils/server/supabase/client";
 
