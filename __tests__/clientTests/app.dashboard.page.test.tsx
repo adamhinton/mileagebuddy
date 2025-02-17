77247c2447fcddc8b5627945b7a47a3693e79be9
adamhinton <adam.d.hinton@gmail.com>
2025-02-17 13:18:05 -0500

    Dashboard test: 	it("Displays vehicle name of each vehicle in the redux store"

--- __tests__/clientTests/app.dashboard.page.test.tsx
+++ __tests__/clientTests/app.dashboard.page.test.tsx
@@ -1,6 +1,10 @@
 import DashboardPage from "@/app/dashboard/page";
 import TestReduxStore from "../testutils/clientTestUtils/reduxtestutils/dummyReduxStore";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+// Not sure why I had to do this here
+// See https://stackoverflow.com/questions/62410948/why-does-jest-dom-give-the-error-typeerror-expect-not-tobevisible-is-not
+import "@testing-library/jest-dom";
 
 describe("Sanity check", () => {
 	it("should be true", () => {
@@ -15,5 +19,18 @@ describe("/dashboard", () => {
 				<DashboardPage />
 			</TestReduxStore>
 		);
+	});
+
+	it("Displays vehicle name of each vehicle in the redux store", () => {
+		render(
+			<TestReduxStore>
+				<DashboardPage />
+			</TestReduxStore>
+		);
+
+		expect(screen.getByText("Ford F-150")).toBeVisible();
+		expect(screen.getByText("Toyota Camry")).toBeVisible();
+		expect(screen.getByText("Tesla Model 3")).toBeVisible();
+		expect(screen.getByText("Nissan Leaf")).toBeVisible();
 	});
 });
