b7dc2cac44f1b79c060872a71ededf733bc8c5f0
adamhinton <adam.d.hinton@gmail.com>
2025-02-17 12:48:04 -0500

    Basic "renders without errors" test for dashboard

---
+++ __tests__/clientTests/app.dashboard.page.test.tsx
@@ -0,0 +1,19 @@
+import DashboardPage from "@/app/dashboard/page";
+import TestReduxStore from "@/redux/dummyReduxStore";
+import { render } from "@testing-library/react";
+
+describe("Sanity check", () => {
+	it("should be true", () => {
+		expect(true).toBe(true);
+	});
+});
+
+describe("/dashboard", () => {
+	it("Renders without errors", () => {
+		render(
+			<TestReduxStore>
+				<DashboardPage />
+			</TestReduxStore>
+		);
+	});
+});
