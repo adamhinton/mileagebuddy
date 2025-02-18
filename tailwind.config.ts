6975ed0471ad174febac79f646d5ff372bee1177
adamhinton <adam.d.hinton@gmail.com>
2025-02-18 18:19:51 -0500

    Add light and dark color theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,15 @@ export default {
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
+	// These aren't accessed directly in the code, but are used in the ... build process, or whatever.
 	theme: {
 		extend: {
 			colors: {
+				primary: "hsl(var(--primary))",
+				secondary: "hsl(var(--secondary))",
+				accent: "hsl(var(--accent))",
+				neutral: "hsl(var(--neutral))",
+				"neutral-text": "hsl(var(--neutral-text))",
 				background: "var(--background)",
 				foreground: "var(--foreground)",
 			},
