96776a66e5ccc12ddc3a3b3a07ba99442aa4be1e
adamhinton <adam.d.hinton@gmail.com>
2025-02-21 13:22:37 -0500

    typo

--- src/app/utils/clientUtils/styling/tailwindClassNames.ts
+++ src/app/utils/clientUtils/styling/tailwindClassNames.ts
@@ -4,7 +4,7 @@
 // These class names come from globals.css. If you change one of those names there, you still have to manually change them here
 // But at least this way there's a single source of truth.
 
-/**Single source of truth for custom tailwind components in globals.css
+/**Single source of truth for custom tailwind component class names in globals.css
  *
  * Is this giving you errors? This class doesn't exist or styling doesn't show up? Make sure the class name corresponds with the custom component defined in globals.css
  */
