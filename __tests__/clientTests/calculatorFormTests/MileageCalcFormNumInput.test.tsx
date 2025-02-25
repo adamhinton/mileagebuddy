69127999a9918229bbffb7705f3ed608aacbc766
adamhinton <adam.d.hinton@gmail.com>
2025-02-25 09:52:55 -0500

    Utils for testing calc form inputs

--- __tests__/clientTests/calculatorFormTests/MileageCalcFormNumInput.test.tsx
+++ __tests__/clientTests/calculatorFormTests/MileageCalcFormNumInput.test.tsx
@@ -1,3 +1,7 @@
+// README
+// This is test for the MileageCalcFormNumInput component (obviously)
+// It shares a lot of logic with MileageCalcFormTextInput, so these two tests share a utils file and many of the tests will look similar
+
 describe("Sanity check", () => {
 	it("should pass", () => {
 		expect(1 + 1).toBe(2);
--- src/app/utils/unitTestUtils/mileageCalculatorFormutils/MileageCalcInputTestUtils.tsx
+++ src/app/utils/unitTestUtils/mileageCalculatorFormutils/MileageCalcInputTestUtils.tsx
@@ -2,3 +2,53 @@
 // This is utils for testing the MileageCalcFormTextInput and MileageCalcFormNumInput components
 // They're very similar, so share much utils logic
 // This is for mocking relevant functions, and creating a wrapper component for testing
+
+import MileageCalcFormNumInput from "@/app/calculator/CalculatorFormComponents/MileageCalcFormNumberInput";
+import MileageCalcFormTextInput from "@/app/calculator/CalculatorFormComponents/MileageCalcFormTextInput";
+import { VehicleForTesting } from "@/app/calculator/page";
+import { Path } from "react-hook-form";
+import { z } from "zod";
+
+const mockRegister = jest.fn().mockReturnValue({
+	onChange: jest.fn(),
+	onBlur: jest.fn(),
+	name: "test",
+});
+
+export const MileageInputNumTestWrapper = ({
+	path,
+	error,
+	schema,
+}: {
+	path: Path<VehicleForTesting>;
+	error?: string;
+	schema: z.ZodNumber;
+}) => {
+	return (
+		<MileageCalcFormNumInput
+			registerFn={mockRegister}
+			path={path}
+			error={error}
+			subSchema={schema}
+		/>
+	);
+};
+
+export const MileageInputTextTestWrapper = ({
+	path,
+	error,
+	schema,
+}: {
+	path: Path<VehicleForTesting>;
+	error?: string;
+	schema: z.ZodString;
+}) => {
+	return (
+		<MileageCalcFormTextInput
+			registerFn={mockRegister}
+			path={path}
+			error={error}
+			subSchema={schema}
+		/>
+	);
+};
