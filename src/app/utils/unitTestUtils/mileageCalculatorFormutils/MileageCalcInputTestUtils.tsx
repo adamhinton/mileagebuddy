8863537b9da25524b4e06c7251ccde7a7573267e
adamhinton <adam.d.hinton@gmail.com>
2025-02-25 09:41:59 -0500

    Better folder structure

---
+++ src/app/utils/unitTestUtils/mileageCalculatorFormutils/MileageCalcInputTestUtils.tsx
@@ -0,0 +1,4 @@
+// README
+// This is utils for testing the MileageCalcFormTextInput and MileageCalcFormNumInput components
+// They're very similar, so share much utils logic
+// This is for mocking relevant functions, and creating a wrapper component for testing
