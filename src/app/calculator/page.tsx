bb3e6b441d8d6956ed26a91533b433f0e86a49ae
adamhinton <adam.d.hinton@gmail.com>
2025-02-28 19:50:09 -0500

    Update comments and format code

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -2,10 +2,11 @@
 
 // README
 // This is the meat and potatoes of the whole MileageBuddy project.
-// This is the Calculator that calculates the cost per mile of a vehicle based on daily use.
+// This is the form where user creates a Vehicle object, which will be used to calculate the cost of driving that vehicle.
+// The form is broken down into sections, each of which corresponds to a sub-object of the Vehicle object.
 // Also calculates the cost of any additional miles driven.
-// This will be used to create (or edit) an object of type Vehicle, whose cost will be calculated with the function from calculateCarCostMain.
 // Form validation will be done by Zod.
+// Form displays input errors next to each input (if there is an error), as well as a summary of the first three sections with errors at the top of the form.
 
 import {
 	Vehicle_For_db_POST,
@@ -33,34 +34,15 @@ import FormErrorSummary from "./CalculatorFormComponents/FormErrorSummary";
 // Edit functionality
 // Animations or something
 // Persist form data on page refresh
-// Three errors at top of form
 // Default values:
 // // Figure out where to save these
 // // Make input default values actually save to form values; right now user has to tab over input
 // More tests
-
-// Styling:
-// Efficient, accessible, clean, responsive. Use color schemes we already have. Update globals.css with new form styling if needed.
+// Make this file a bit smaller, abstractify some stuff
 
 // SPECS:
-// Frontend: react-hook-form (I think?) with zod validation. Then submit to server action; once that comes back happy, update redux state.
 // Stretch: optimistic UI updates
-// Backend: server actions, also with zod validation. Then submit created (or patched) vehicle to /api/vehicles
-// Zod validation on both client and server
-// There will be lots of inputs, figure out how to break that up
-// Multiple pages? Like one for each sub-object of vehicle? (yearlyMaintenanceCosts, yearlyFixedCosts etc)
-// Or collapsible sections?
-// Definitely not just one long form
-// Input error specs:
-// Line-item errors on bad input, probably in red right above that inut
-// Focus page on the first error if it pops up after submit
-// Ideally show error before they hit submit
-// Display error summary at top of form too; ideally clickable ones that take user to relevant section
-
 // Specifics of form inputs:
-// Radio button near the start for gas vs electric
-// Work out how to update form state for gas vs electric
-// Make sure units are very clear (dollars, gallons etc)
 
 // TYPES/VALIDATION
 // Vehicle is DeepReadOnly right now, need to make mutable version for this which will be easy
@@ -68,24 +50,8 @@ import FormErrorSummary from "./CalculatorFormComponents/FormErrorSummary";
 // Use Vehicle_For_DB_PATCH and VehicleSchemaForPATCH when editing a vehicle using this form
 // Note: VehicleToBePostedSchema is a union of GasVehicleSchemaForPOST and ElectricVehicleSchemaForPOST. Will have to do validation on one or the other based on user input, you can't do zod validation on a union type. but that shouldn't be too hard.
 
-// INCREMENTAL CODING:
-// Start with a very basic form
-// Make a VehiclToBePostedSchema extension that just has one sub-object, maybe yearlyFixedCosts
-// Get that up and running, then gradually add more
-// Early on we want to make sure we can account for the difference between gas vehicles and EVs. There are already schemas for this - GasVehicleSchemaForPOST, ElectricVehicleSchemaForPOST -- but we need to make sure we have the form logic worked out for that.
-// User will select "electric" or "gas" at the start of the form. The only difference between the two is whether there's a section for the sub-object gasVehicleData or ElectricVehicleData, shouldn't be too hard
-
-// COMPONENTS:
-// We'll make reusable tailwind components because a lot of these are going to be same-y
-// Reusable number component
-// I don't believe anything will ever be more than six digits long
-// Reusable short form text input
-// I don't believe there will ever be any long text inputs
-
 // STRETCH:
 // Real time calculations? Maybe broken down by section.
-
-// Will start by writing the form for just one or two sections, thene expand from there
 
 const CalculatorPage = () => {
 	return (
@@ -102,6 +68,7 @@ export default CalculatorPage;
 const CalculateMileageForm = () => {
 	// Checkmark that user de-selects if they don't want to be auto-advanced to next section
 	const [autoAdvance, setAutoAdvance] = useState(true);
+	// Show the error summary at the top of the form
 	const [isShowErrorSummary, setisShowErrorSummary] = useState(false);
 
 	const form = useForm<Vehicle_For_db_POST>({
@@ -171,6 +138,7 @@ const CalculateMileageForm = () => {
 				</label>
 			</div>
 
+			{/* If vehicle is gas, display gas vehicle questions. If electric, display EV questions */}
 			{watchedVehicleType === "gas" ? (
 				<GasVehicleDataSubForm
 					register={register}
@@ -188,16 +156,20 @@ const CalculateMileageForm = () => {
 			) : null}
 
 			{/* User has to specify vehicle type (gas or electric) before seeing the rest of the form
-			
-			// Each of these sub-forms is a sub-object of Vehicle_For_db_POST
+			 Each of these sub-forms is a sub-object of Vehicle_For_db_POST
 			*/}
 			{watchedVehicleType && (
 				<>
 					<VehicleDataSubForm register={register} errors={errors} />
+
 					<PurchaseAndSalesSubForm register={register} errors={errors} />
+
 					<UsageSubForm register={register} errors={errors} />
+
 					<FixedCostsSubForm register={register} errors={errors} />
+
 					<YearlyMaintenanceCostsSubForm register={register} errors={errors} />
+
 					<VariableCostsSubForm register={register} errors={errors} />
 
 					<button
