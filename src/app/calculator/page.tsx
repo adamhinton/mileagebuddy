a5875996c1ff4170c5be4bc3938001ec825f0826
adamhinton <adam.d.hinton@gmail.com>
2025-03-02 13:45:13 -0500

    Re-organize form page

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -8,17 +8,16 @@
 // Form validation will be done by Zod.
 // Form displays input errors next to each input (if there is an error), as well as a summary of the first three sections with errors at the top of the form.
 
+import { useState, useMemo } from "react";
+import { useForm, FieldErrors } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
 	Vehicle_For_db_POST,
 	VehicleToBePostedSchema,
 	Gas_Vehicle_For_DB_POST,
 	Electric_Vehicle_For_DB_POST,
 } from "../utils/server/types/VehicleTypes/POSTVehicleTypes";
-import { useMemo } from "react";
 import mySubmitLogic from "./formActions";
-import { FieldErrors, useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
 import VehicleDataSubForm from "./CalculatorFormComponents/FormSubSections/VehicleDataSubForm";
 import PurchaseAndSalesSubForm from "./CalculatorFormComponents/FormSubSections/PurchaseAndSalesSubForm";
 import UsageSubForm from "./CalculatorFormComponents/FormSubSections/UsageSubForm";
@@ -33,52 +32,31 @@ import {
 	useFormNavigation,
 } from "./calculatorUtils/FormNavUtils";
 
-// TODO:
-// Next buttons? Tie to (un)collapsing
-// // Maybe delete auto-advance checkbox if so
-// Edit functionality
-// Animations or something
-// Persist form data on page refresh
-// Default values:
-// // Figure out where to save these
-// // Make input default values actually save to form values; right now user has to tab over input
-// More tests
-// Make this file a bit smaller, abstractify some stuff
-
-// SPECS:
-// Stretch: optimistic UI updates
-// Specifics of form inputs:
-
 // TYPES/VALIDATION
 // Vehicle is DeepReadOnly right now, need to make mutable version for this which will be easy
-// Use VehicleToBePostedSchema from zod and the ts type Vehicle_For_DB_POST here. That will be the object which guides the zod validaton. It omits things like vehicleID etc.
+// Use VehicleToBePostedSchema from zod and the ts type Vehicle_For_DB_POST here. That will be the object which guides the zod validation. It omits things like vehicleID etc.
 // Use Vehicle_For_DB_PATCH and VehicleSchemaForPATCH when editing a vehicle using this form
 // Note: VehicleToBePostedSchema is a union of GasVehicleSchemaForPOST and ElectricVehicleSchemaForPOST. Will have to do validation on one or the other based on user input, you can't do zod validation on a union type. but that shouldn't be too hard.
 
-// STRETCH:
-// Real time calculations? Maybe broken down by section.
+/**Better way of toggling collapsed state than true or false */
+export type CollapsedOrNot = "isCollapsed" | "isNotCollapsed";
 
 const CalculatorPage = () => {
 	return (
 		<section className="h-screen p-4 sm:p-6 md:p-8">
 			<h1 className="text-2xl sm:text-3xl md:text-4xl">Calculator Page</h1>
-
 			<CalculateMileageForm />
 		</section>
 	);
 };
 
-export default CalculatorPage;
-
-/**Better way of toggling collapsed state than true or false */
-export type CollapsedOrNot = "isCollapsed" | "isNotCollapsed";
-
 const CalculateMileageForm = () => {
-	// Determines whether each section is collapsed or not
+	// User can collapse or uncollapse form sections
+	// Hitting "next" in a section also scrolls to the next section and uncollapses it
+	// This state (obviously) tracks which sections are collapsed or not)
 	const [collapsedSections, setCollapsedSections] = useState<
 		Record<CollapsibleSectionTitles, CollapsedOrNot>
 	>({
-		// Set initial collapsed state for each section except the first
 		gasVehicleData: "isNotCollapsed",
 		vehicleData: "isCollapsed",
 		electricVehicleData: "isCollapsed",
@@ -89,24 +67,17 @@ const CalculateMileageForm = () => {
 		variableCosts: "isCollapsed",
 	});
 
-	// Checkmark that user de-selects if they don't want to be auto-advanced to next section
-	const [autoAdvance, setAutoAdvance] = useState(true);
-
-	// Show the error summary at the top of the form
+	/**Shows bullet-pointed errors at top of form after user hits submit */
 	const [isShowErrorSummary, setisShowErrorSummary] = useState(false);
 
+	// Form setup
 	const form = useForm<Vehicle_For_db_POST>({
-		// This is how it knows to validate with zod
+		// This is how you tell rhf to use zod for validation
 		resolver: zodResolver(VehicleToBePostedSchema),
-		// Can put default values here if needed: defaultValues: {...}
 	});
 
 	const {
 		register,
-		/**
-		 * built in RHF function that automatically performs zod validation,
-		 * then passes the form data to whatever you pass as a callback in <form onSubmit={mySubmitFunction(callback)}>
-		 */
 		handleSubmit,
 		setValue,
 		getValues,
@@ -116,11 +87,13 @@ const CalculateMileageForm = () => {
 
 	const formValues = getValues();
 
-	// Using this instead of formValues.type because this way the form re-renders when watchedVehicleType changes
+	/**Changes the displayed form section when user indicates that they have a gas or EV
+	 *
+	 * Can't just use formValues.type for this because the component won't re-render when that changes
+	 */
 	const watchedVehicleType = watch("type");
 
-	// The order form sections are displayed in
-	// Don't totally understand why this needs to be a useMemo, but linter yells at me in the useCallback below if I don't
+	/**The order form sections are listed in, for form navigation purposes */
 	const formSectionOrder = useMemo(() => {
 		const formSectionOrder: Readonly<CollapsibleSectionTitles[]> = [
 			watchedVehicleType === "gas" ? "gasVehicleData" : "electricVehicleData",
@@ -144,24 +117,11 @@ const CalculateMileageForm = () => {
 	console.log("formValues inside form before submit:", formValues);
 	console.log("errors inside form before submit:", errors);
 
-	// Comes first by default
+	// new vehicles should always come first in user's list
 	setValue("vehiclesOrder", 1);
 
 	return (
 		<form onSubmit={handleSubmit(mySubmitLogic)}>
-			<div className="mb-4 flex items-center">
-				<input
-					type="checkbox"
-					id="autoAdvance"
-					checked={autoAdvance}
-					onChange={(e) => setAutoAdvance(e.target.checked)}
-					className="mr-2"
-				/>
-				<label htmlFor="autoAdvance">
-					Automatically advance to next section
-				</label>
-			</div>
-
 			{isShowErrorSummary && <FormErrorSummary errors={errors} />}
 
 			<div className="ml-4">
@@ -212,9 +172,7 @@ const CalculateMileageForm = () => {
 				/>
 			) : null}
 
-			{/* User has to specify vehicle type (gas or electric) before seeing the rest of the form
-			 Each of these sub-forms is a sub-object of Vehicle_For_db_POST
-			*/}
+			{/* User has to specify vehicle type (gas or electric) before seeing the rest of the form */}
 			{watchedVehicleType && (
 				<>
 					<VehicleDataSubForm
@@ -294,3 +252,5 @@ const CalculateMileageForm = () => {
 		</form>
 	);
 };
+
+export default CalculatorPage;
