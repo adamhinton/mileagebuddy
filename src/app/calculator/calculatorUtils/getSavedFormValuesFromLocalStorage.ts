9c1fe3355943be442c1a65d46c5ea3b12dc46a0a
adamhinton <adam.d.hinton@gmail.com>
2025-03-11 10:55:19 -0400

    Fix: localStorage form values weren't being accessed beacuse of bad import path

--- src/app/calculator/calculatorUtils/getSavedFormValuesFromLocalStorage.ts
+++ src/app/calculator/calculatorUtils/getSavedFormValuesFromLocalStorage.ts
@@ -1,4 +1,5 @@
-import { LOCAL_STORAGE_FORM_DATA_KEY } from "../page";
+import { VehicleToBePostedSchema } from "@/app/utils/server/types/VehicleTypes/POSTVehicleTypes";
+import { LOCAL_STORAGE_FORM_DATA_KEY } from "../CalculatorFormComponents/VehicleCreationForm";
 
 /**When the user navigates away, any in-progress form values are saved to local storage and retrieved when they come back
  * This function retrieves those values on page load
@@ -7,6 +8,8 @@ const getSavedFormValuesFromLocalStorage = () => {
 	const savedData = localStorage.getItem(LOCAL_STORAGE_FORM_DATA_KEY);
 	if (savedData) {
 		try {
+			// Not doing Zod validation here because the form isn't complete yet, and the user could have saved invalid data
+			// If the user tries to submit the form, the form will be validated then, so I don't believe this is a security risk
 			return JSON.parse(savedData);
 		} catch (e) {
 			console.error("Error parsing saved form data:", e);
