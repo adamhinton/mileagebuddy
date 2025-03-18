14c53413a5ad740d1722a1aed8893ebd73f250a6
adamhinton <adam.d.hinton@gmail.com>
2025-03-18 12:40:24 -0400

    Update docs

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
+// _______________________________________________________
+// Dashboard component displaying user's saved Vehicles
+// Shows a list of vehicles with calculated costs per mile
+// Has Drag and Drop functionality for reordering vehicles
+// Edit and delete buttons for each vehicle
+// _______________________________________________________
+
 "use client";
 
 import { useState, useCallback, useEffect } from "react";
