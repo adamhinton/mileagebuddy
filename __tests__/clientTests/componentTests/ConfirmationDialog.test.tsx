10010c187b2c8a2ba167a3c8baec63f5b532e942
adamhinton <adam.d.hinton@gmail.com>
2025-03-18 12:31:42 -0400

    Skeleton ConfirmationDialog.test.tsx

---
+++ __tests__/clientTests/componentTests/ConfirmationDialog.test.tsx
@@ -0,0 +1,4 @@
+// _________________________________________________________________
+// This is (obviously) tests for ConfirmationDialog.tsx
+// This component typically appears nested in Button.tsx
+// _________________________________________________________________
