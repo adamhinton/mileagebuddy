0b3f2e844cc77b381e7c480e3b9ec8c5af198c17
adamhinton <adam.d.hinton@gmail.com>
2025-03-21 00:36:54 -0400

    Dummy VehicleCard.test

---
+++ __tests__/clientTests/Dashboardtests/VehicleCard.test.tsx
@@ -0,0 +1,12 @@
+// __________________________________________________________
+// This is (obviously) tests for VehicleCard.tsx
+// This is a component that displays a vehicle card with its calculated costs
+// Also contains edit and delete buttons
+// It has drag and drop functionality, but that's handled by its parent component SortableVehicleCard.tsx, so that isn't tested here
+// __________________________________________________________
+
+describe("Sanity check", () => {
+	it("Sanity check", () => {
+		expect(true).toBe(true);
+	});
+});
