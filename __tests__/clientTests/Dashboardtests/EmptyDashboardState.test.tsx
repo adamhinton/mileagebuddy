07b1215ed2f483483051bf0fe5d341ea16214947
adamhinton <adam.d.hinton@gmail.com>
2025-03-21 15:09:39 -0400

    Add tests for EmptyDashboardState.tsx

---
+++ __tests__/clientTests/Dashboardtests/EmptyDashboardState.test.tsx
@@ -0,0 +1,36 @@
+// _______________________________________________________________
+// This is (obviously) tests for EmptyDashboardState.tsx
+// This component is fairly simple so there isn't too much to test
+// ______________________________________________________________
+
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import EmptyDashboardState from "@/app/dashboard/components/EmptyDashboardState";
+
+describe("EmptyDashboardState", () => {
+	it("renders without errors", () => {
+		render(<EmptyDashboardState />);
+	});
+
+	it("displays the correct heading", () => {
+		render(<EmptyDashboardState />);
+
+		const heading = screen.getByText(/No vehicles yet/i);
+		expect(heading).toBeVisible();
+	});
+
+	it("displays the correct paragraph text", () => {
+		render(<EmptyDashboardState />);
+		const paragraph = screen.getByText(
+			/Add your first vehicle to calculate its true cost per mile/i
+		);
+		expect(paragraph).toBeVisible();
+	});
+
+	it("contains a link to the calculator page", () => {
+		render(<EmptyDashboardState />);
+
+		const link = screen.getByRole("link", { name: /Add Vehicle/i });
+		expect(link).toBeVisible();
+	});
+});
