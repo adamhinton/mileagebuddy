da26f0ceea28b6d546a0c8aec93533769fd2b152
adamhinton <adam.d.hinton@gmail.com>
2025-03-27 12:49:47 -0400

    Update tests to reflect new edit functionality

--- __tests__/clientTests/Dashboardtests/app.dashboard.page.test.tsx
+++ __tests__/clientTests/Dashboardtests/app.dashboard.page.test.tsx
@@ -7,7 +7,26 @@
 
 import DashboardPage from "@/app/dashboard/page";
 import TestReduxStore from "@/app/utils/unitTestUtils/dummyReduxStore";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+
+// Not sure why I had to do this here
+// See https://stackoverflow.com/questions/62410948/why-does-jest-dom-give-the-error-typeerror-expect-not-tobevisible-is-not
+import "@testing-library/jest-dom";
+
+// Added this because Dashboard uses router for navigation, and the test renders were getting errors without it
+jest.mock("next/navigation", () => ({
+	useRouter: jest.fn(() => ({
+		push: jest.fn(),
+		replace: jest.fn(),
+		prefetch: jest.fn(),
+		back: jest.fn(),
+		forward: jest.fn(),
+		refresh: jest.fn(),
+		// Add other router properties or methods as needed for your tests
+	})),
+	usePathname: jest.fn(() => "/"),
+	useSearchParams: jest.fn(() => new URLSearchParams()),
+}));
 
 describe("dashboard/page.tsx", () => {
 	it("Renders without crashing when passed a list of vehicles", () => {
@@ -26,5 +45,21 @@ describe("dashboard/page.tsx", () => {
 				<DashboardPage />
 			</TestReduxStore>
 		);
+	});
+
+	it("Displays vehicle name of each vehicle in the redux store", () => {
+		render(
+			<TestReduxStore>
+				<DashboardPage />
+			</TestReduxStore>
+		);
+
+		expect(screen.getByText("Toyota Camry")).toBeVisible();
+		expect(screen.getByText("Tesla Model 3")).toBeVisible();
+		expect(screen.getByText("Nissan Leaf")).toBeVisible();
+
+		// Sanity check
+		const shouldntExist = "This vehicle shouldn't exist";
+		expect(screen.queryByText(shouldntExist)).toBeNull();
 	});
 });
