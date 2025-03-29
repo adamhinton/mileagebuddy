4718a912e9f07e2ae8305a0a9549825adc4b2046
adamhinton <adam.d.hinton@gmail.com>
2025-03-29 00:00:13 -0400

    Update tests to provide context for router. All tests passing

--- __tests__/clientTests/calculatorFormTests/app.calculator.page.test.tsx
+++ __tests__/clientTests/calculatorFormTests/app.calculator.page.test.tsx
@@ -2,6 +2,21 @@ import CalculatorPage from "@/app/calculator/page";
 import TestReduxStore from "@/app/utils/unitTestUtils/dummyReduxStore";
 import { render } from "@testing-library/react";
 
+// Added this because form submit logic uses router to push to /dashboard on success, and the test renders were getting errors without it
+jest.mock("next/navigation", () => ({
+	useRouter: jest.fn(() => ({
+		push: jest.fn(),
+		replace: jest.fn(),
+		prefetch: jest.fn(),
+		back: jest.fn(),
+		forward: jest.fn(),
+		refresh: jest.fn(),
+		// Add other router properties or methods as needed for your tests
+	})),
+	usePathname: jest.fn(() => "/"),
+	useSearchParams: jest.fn(() => new URLSearchParams()),
+}));
+
 describe("Sanity check", () => {
 	it("should pass", () => {
 		expect(1 + 1).toBe(2);
