cbd8e73374082e1979e3910f88f268939dece7b2
adamhinton <adam.d.hinton@gmail.com>
2025-03-29 11:56:18 -0400

    Make Tabs more visually distinct from Header

--- src/components/Header/HeaderSubComponents/Tabs.tsx
+++ src/components/Header/HeaderSubComponents/Tabs.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 // Has links to all internal pages
 // Thanks to Flowbite, whom I shamelessly ripped this off from:
 // https://flowbite.com/docs/components/tabs/
+// This is toggleable. It's also collapsed by default on
 
 // TODO tabs:
 // -- Delineate this visually from Header.tsx
@@ -19,48 +20,45 @@ import Link from "next/link";
 
 const Tabs = () => {
 	return (
-		<ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-			<li className="me-2">
-				<Link
-					href="/login"
-					aria-current="page"
-					className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500"
-				>
-					Login
-				</Link>
-			</li>
-			<li className="me-2">
-				<Link
-					href="/dashboard"
-					className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-				>
-					Dashboard
-				</Link>
-			</li>
-			<li className="me-2">
-				<Link
-					href="/calculator"
-					className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-				>
-					Calculator
-				</Link>
-			</li>
-			<li className="me-2">
-				<Link
-					href="/"
-					className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-				>
-					{/* TODO about page */}
-					About
-				</Link>
-			</li>
-			{/* Example of disabled */}
-			{/* <li>
-				<Link className="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">
-					Disabled
-				</Link>
-			</li> */}
-		</ul>
+		<section className="bg-background-elevated py-2 shadow-sm">
+			<div className="container mx-auto px-4">
+				<ul className="flex flex-wrap text-sm font-medium text-center text-neutral-text border-b border-primary-50 dark:border-primary-100/30">
+					<li className="me-2">
+						<Link
+							href="/login"
+							aria-current="page"
+							className="inline-block p-3 bg-primary-50 text-primary rounded-t-lg border-b-2 border-primary dark:bg-primary-100/20 dark:border-primary-200"
+						>
+							Login
+						</Link>
+					</li>
+					<li className="me-2">
+						<Link
+							href="/dashboard"
+							className="inline-block p-3 rounded-t-lg hover:bg-background-highlight hover:text-primary transition-colors"
+						>
+							Dashboard
+						</Link>
+					</li>
+					<li className="me-2">
+						<Link
+							href="/calculator"
+							className="inline-block p-3 rounded-t-lg hover:bg-background-highlight hover:text-primary transition-colors"
+						>
+							Calculator
+						</Link>
+					</li>
+					<li className="me-2">
+						<Link
+							href="/"
+							className="inline-block p-3 rounded-t-lg hover:bg-background-highlight hover:text-primary transition-colors"
+						>
+							About
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</section>
 	);
 };
 
