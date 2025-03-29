ab1bcce5e6390992cdd5e985e0171da14ab21d7f
adamhinton <adam.d.hinton@gmail.com>
2025-03-29 11:27:41 -0400

    Skeleton Tabs.tsx

---
+++ src/components/Header/HeaderSubComponents/Tabs.tsx
@@ -0,0 +1,10 @@
+// ________________________________________________________________________
+// This is tabs that handle internal navigation
+// Has links to all internal pages
+
+// TODO tabs:
+// -- Figure out where exactly to put this
+// -- Organize styling
+// -- Change navLinks to have external links (github linkedin etc) since internal is handled by Tabs
+// -- Unit tests
+// ________________________________________________________________________
