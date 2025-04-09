911a162cb3205c285a778c710f00d7abb4b52578
adamhinton <adam.d.hinton@gmail.com>
2025-04-09 11:50:21 -0400

    Deprecate old landing page in favor of /about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,33 @@
 // _____________________________________________________________________
-// This is the about/landing page
-// Has basic info about the app and how to use it
+// This is the root URL (/)
+// All it does is push the user to the about page, or dashboard if they're already logged in and have Vehicles
+// TODO Stretch put this routing logic in middleware, I just did it this way because I was tired
 // _____________________________________________________________________
 
 "use client";
 
 import { useAppSelector } from "@/redux/hooks";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
 	const loggedInUser = useAppSelector((state) => state.user.value);
 	const isLoggedIn = !!loggedInUser?.id;
 
 	const usersVehicles = useAppSelector((state) => state.vehicles);
-	console.log("usersVehicles:", usersVehicles);
 
-	return <main>About!</main>;
+	const router = useRouter();
+
+	if (!isLoggedIn) {
+		router.push("/about");
+	}
+
+	if (!usersVehicles || usersVehicles.length === 0) {
+		router.push("/about");
+	}
+
+	if (usersVehicles.length > 0) {
+		router.push("/dashboard");
+	}
+
+	return <main></main>;
 }
