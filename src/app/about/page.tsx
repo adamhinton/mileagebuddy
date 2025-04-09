facccea766146f361ce87079fc1b57715c07164b
adamhinton <adam.d.hinton@gmail.com>
2025-04-09 11:50:43 -0400

    Skeleton About page and TableOfContents - will fill in momentarily

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+// _____________________________________________________________________________
+// Ths is the About page (obviously)
+// Has a table of contents, basic description of the app and its benefits, and a quick how-to guide
+// _____________________________________________________________________________
+
+const AboutPage = () => {
+	return <div>About Page</div>;
+};
+
+export default AboutPage;
