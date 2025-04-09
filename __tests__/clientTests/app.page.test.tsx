787dd757a32ffce9b842b13e30cb8e380c169245
adamhinton <adam.d.hinton@gmail.com>
2025-04-09 11:57:17 -0400

    Fix failing tests due to about page changes

--- __tests__/clientTests/app.page.test.tsx
+++ __tests__/clientTests/app.page.test.tsx
@@ -4,6 +4,22 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import TestReduxStore from "../../src/app/utils/unitTestUtils/dummyReduxStore";
 
+// Added this because form submit logic uses router to push to /dashboard on success, and the test renders were getting errors without it
+// Fixes the error: "Invariant: Expected app router to be mounted"
+jest.mock("next/navigation", () => ({
+	useRouter: jest.fn(() => ({
+		push: jest.fn(),
+		replace: jest.fn(),
+		prefetch: jest.fn(),
+		back: jest.fn(),
+		forward: jest.fn(),
+		refresh: jest.fn(),
+		// Add other router properties or methods as needed for your tests
+	})),
+	usePathname: jest.fn(() => "/"),
+	useSearchParams: jest.fn(() => new URLSearchParams()),
+}));
+
 describe("Page", () => {
 	it("renders without errors", () => {
 		render(
