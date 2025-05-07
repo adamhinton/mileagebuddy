47efdb42bc0ee1b4741db526f83d5fb1d9d5e7d2
adamhinton <adam.d.hinton@gmail.com>
2025-05-07 13:45:57 -0400

    Better organization of trip schemas

--- src/app/zod/schemas/trips/TripSchemas/BaseTripSchemas.ts
+++ src/app/zod/schemas/trips/TripSchemas/BaseTripSchemas.ts
@@ -2,17 +2,30 @@
 // This is schemas for the Trip features
 // Users input data about a trip they want to take, and it helps them understand the cost of various transportation options
 //    - Based on whether they take their own vehicle, public transit, flight etc
+// Schemas and types for POST and PATCH are defined elsewhere in this directory
+
+// NOTE: We are only concerned with items that do the following:
+//    - Help the user understand their trip
+//    - Help the app calculate the cost of a particular trip option
+// So we don't care about things like hotel or food costs, etc.
+
+// Trips are divided into two types:
+//    - Short distance trips
+//    - Long distance trips
+
+// Each trip can have multiple TripOptions
+//    - Each TripOption is a different way to take the trip
 // _________________________________________________________________
 
 import { z } from "zod";
-import { TripOptionSchema } from "./TripOptionSchemas";
+import { TripOptionSchema } from "../TripOptionSchemas";
 import { DeepReadonly } from "next/dist/shared/lib/deep-readonly";
 
 /**
  * Don't use this Schema
  * It's the grandaddy schema that we infer (and extend) trip types for DB post, patch, get etc, as well as for form validation
  */
-export const BaseTripSchema = z.object({
+export const _BaseTripSchema = z.object({
 	// Basic details, common to all trips
 	// This stuff isn't used for calculations - only for helping the user understand their trip
 	// So we're liberal in what inputs we accept here
@@ -38,7 +51,7 @@ export const BaseTripSchema = z.object({
 		.nullish()
 		.default("Home")
 		.describe("Origin (Default: Home)"),
-	notes: z.string().max(500).nullish().describe("Notes"),
+	notes: z.string().trim().max(500).nullish().describe("Notes"),
 
 	// Fields relevant for calculation (stricter validation where applicable)
 	// User defines this, influences available/relevant options/UI
@@ -59,16 +72,20 @@ export const BaseTripSchema = z.object({
 /**
  * This is the schema for short distance trips
  * It's combined with the long distance schema to make a single schema
+ *
+ * Has slightly fewer fields than long distance
  */
-export const ShortDistanceTripSchema = BaseTripSchema.extend({
+export const ShortDistanceTripSchema = _BaseTripSchema.extend({
 	tripType: z.literal("SHORT_DISTANCE"),
 });
 
 /**
  * This is the schema for long distance trips
  * It's combined with the short distance schema to make a single schema
+ *
+ * Has slightly more fields than short distance
  */
-export const LongDistanceTripSchema = BaseTripSchema.extend({
+export const LongDistanceTripSchema = _BaseTripSchema.extend({
 	tripType: z.literal("LONG_DISTANCE"),
 	departureDate: z.coerce.date().nullish().describe("Departure Date"),
 	returnDate: z.coerce.date().nullish().describe("Return Date"),
@@ -93,68 +110,9 @@ export const TripSchema = z.discriminatedUnion("tripType", [
 export type Trip = DeepReadonly<z.infer<typeof TripSchema>>;
 
 // Have to use this in several places so keeping this DRY
-const UserIdSchema = z.string().uuid().describe("User ID");
-
-/**
- * This is combined with long distance schema to make a single schema for POST requests
- */
-const ShortDistanceTripSchemaForPOST = ShortDistanceTripSchema.extend({
-	// Need userid for creation
-	userid: UserIdSchema,
-});
-
-/**
- * This is combined with short distance schema to make a single schema for POST requests
- */
-const LongDistanceTripSchemaForPOST = LongDistanceTripSchema.extend({
-	// Need userid for creation
-	userid: UserIdSchema,
-});
+export const UserIdSchema = z.string().uuid().describe("User ID");
 
 /**
  * Defining an ID schema for consistency because this will be used in several places
  */
-const TripIDSchema = z.string().uuid().describe("Trip ID");
-
-/**Schema for validating TRIP to be posted to DB
- *
- * Had to define schemas and combine them here because zod doesn't just let you add userid to the base schema,
- * because the base schema is already a discriminated union
- *
- * Which is incredibly stupid, but whatever
- */
-export const TripSchemaForPOST = z.discriminatedUnion("tripType", [
-	ShortDistanceTripSchemaForPOST,
-	LongDistanceTripSchemaForPOST,
-]);
-export type Trip_For_DB_POST = DeepReadonly<z.infer<typeof TripSchemaForPOST>>;
-
-/**
- * This will be combined with long distance schema to make a single schema for PATCH requests
- */
-const ShortDistanceTripSchemaForPATCH = ShortDistanceTripSchema.extend({
-	userid: UserIdSchema,
-	tripID: TripIDSchema,
-});
-
-/**
- * This will be combined with short distance schema to make a single schema for PATCH requests
- */
-const LongDistanceTripSchemaForPATCH = LongDistanceTripSchema.extend({
-	userid: UserIdSchema,
-	tripID: TripIDSchema,
-});
-
-/**
- * Schema for validating TRIP to be patched in DB
- *
- * All fields are included - must pass in entire Trip object - but only the ones that are actually changed will be updated
- *
- * We don't need to pass in a partial because Trips don't have that many details and the user won't have that many Trips,
- *
- * so hyper-optimization isn't necessary
- */
-export const TripSchemaForPATCH = z.discriminatedUnion("tripType", [
-	ShortDistanceTripSchemaForPATCH,
-	LongDistanceTripSchemaForPATCH,
-]);
+export const TripIDSchema = z.string().uuid().describe("Trip ID");
---
+++ src/app/zod/schemas/trips/TripSchemas/TripSchemaForPatch.ts
@@ -0,0 +1,42 @@
+// _________________________________________________________________
+// This is Trip schemas and TS types for DB PATCH requests
+// _________________________________________________________________
+
+import {
+	LongDistanceTripSchema,
+	ShortDistanceTripSchema,
+	TripIDSchema,
+	UserIdSchema,
+} from "./BaseTripSchemas";
+
+import { z } from "zod";
+
+/**
+ * This will be combined with long distance schema to make a single schema for PATCH requests
+ */
+const ShortDistanceTripSchemaForPATCH = ShortDistanceTripSchema.extend({
+	userid: UserIdSchema,
+	tripID: TripIDSchema,
+});
+
+/**
+ * This will be combined with short distance schema to make a single schema for PATCH requests
+ */
+const LongDistanceTripSchemaForPATCH = LongDistanceTripSchema.extend({
+	userid: UserIdSchema,
+	tripID: TripIDSchema,
+});
+
+/**
+ * Schema for validating TRIP to be patched in DB
+ *
+ * All fields are included - must pass in entire Trip object - but only the ones that are actually changed will be updated
+ *
+ * We don't need to pass in a partial because Trips don't have that many details and the user won't have that many Trips,
+ *
+ * so hyper-optimization isn't necessary
+ */
+export const TripSchemaForPATCH = z.discriminatedUnion("tripType", [
+	ShortDistanceTripSchemaForPATCH,
+	LongDistanceTripSchemaForPATCH,
+]);
