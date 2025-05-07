6c40e1227ed07f362a89d910c9076da181af6ea3
adamhinton <adam.d.hinton@gmail.com>
2025-05-07 14:09:44 -0400

    Typo

--- src/app/zod/schemas/trips/TripSchemas/TripOptionSchemas/BaseTripOptionSchemas.ts
+++ src/app/zod/schemas/trips/TripSchemas/TripOptionSchemas/BaseTripOptionSchemas.ts
@@ -3,7 +3,7 @@
 // Each Trip object can have multiple TripOptions
 //    - Each TripOption is a different way to take the trip
 //    - Each TripOption has a different cost
-//    - One trip option mightnbe a flight, one might be a car, etc
+//    - One trip option might be a flight, one might be a car, etc
 //    - This app isn't actually concerned with the mode of travel, but rather the cost of each option
 // _________________________________________________________________
 
