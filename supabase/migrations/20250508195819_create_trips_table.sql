ad85724b2b57312db6a033130b438a9ec7452d4f
adamhinton <adam.d.hinton@gmail.com>
2025-05-08 16:24:19 -0400

    Finalized Trips db table

---
+++ supabase/migrations/20250508195819_create_trips_table.sql
@@ -0,0 +1,28 @@
+-- ___________________________________________________________
+-- This is for the Trips feature
+-- It corresponds to TripSchema
+-- ___________________________________________________________
+
+DROP TABLE IF EXISTS trips;
+
+CREATE TABLE trips (
+    -- Stuff not used in cost calculation
+    id SERIAL PRIMARY KEY,
+    userID uuid NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (userID) REFERENCES auth.users(id) ON DELETE CASCADE,
+
+    -- These help the user classify and understand
+    name VARCHAR(100) NOT NULL,
+    destination VARCHAR(100) NOT NULL,
+    origin VARCHAR(100),
+    notes VARCHAR(500),
+
+    -- Now trip cost calculation stuff
+    tripType VARCHAR(20) NOT NULL CHECK(tripType IN ('SHORT_DISTANCE', 'LONG_DISTANCE')),
+    roundTripDrivingDistanceMiles DECIMAL(7, 1),
+    
+    -- Can have multiple TripOptions - one to many relationship - that relationship is modeled in TripOption's foreign key
+
+    "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP  
+);
