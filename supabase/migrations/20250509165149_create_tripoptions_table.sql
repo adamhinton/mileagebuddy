d03cfa24e9d7583a11ce33083259a434f3b82e5b
adamhinton <adam.d.hinton@gmail.com>
2025-05-09 12:54:23 -0400

    skeleton tripoptions migration

---
+++ supabase/migrations/20250509165149_create_tripoptions_table.sql
@@ -0,0 +1,6 @@
+-- ___________________________________________________________
+-- This is for the Trips feature
+-- Each Trip can have multiple TripOptions
+-- A one-to-many relationship
+-- ___________________________________________________________
+
