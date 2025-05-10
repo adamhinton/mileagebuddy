1301c97508c7ed27af6bc204c0a8eca31d1901f4
adamhinton <adam.d.hinton@gmail.com>
2025-05-10 12:42:52 -0400

    Create skeleton Trips UI components

--- src/app/trip/page.tsx
+++
@@ -1,5 +0,0 @@
-const TripPage: React.FC = () => {
-	return <div className="min-h-screen">blah blah blah</div>;
-};
-
-export default TripPage;
---
+++ src/app/trip_planner/TripFormComponents/TripOptionCreationOrEditForm.tsx
@@ -0,0 +1,4 @@
+// __________________________________________________________________________________
+// This is (obviously) the form where user can edit or create a trip Option.
+// Each Trip can have multiple TripOptions, so this form will be able to appear more than once in TripCreationOrEditForm.
+// __________________________________________________________________________________
