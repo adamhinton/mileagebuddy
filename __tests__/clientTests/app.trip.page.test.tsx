a7b795eb719347a821875444b0cb5e32b8e0f049
adamhinton <adam.d.hinton@gmail.com>
2025-05-17 14:38:24 -0400

    Fix type errors that were causing my deployment to fail

--- __tests__/clientTests/app.trip.page.test.tsx
+++ __tests__/clientTests/app.trip.page.test.tsx
@@ -1,22 +1,21 @@
-import TripPage from "@/app/trip/page";
-
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import TestReduxStore from "../../src/app/utils/unitTestUtils/dummyReduxStore";
+import TripCreationPage from "@/app/trip_planner/page";
 
 describe("TripPage", () => {
 	it("renders without errors", () => {
 		render(
 			<TestReduxStore>
-				<TripPage />
+				<TripCreationPage />
 			</TestReduxStore>
 		);
 	});
-	
+
 	it("matches snapshot from 4.30.2025", () => {
 		const { container } = render(
 			<TestReduxStore>
-				<TripPage />
+				<TripCreationPage />
 			</TestReduxStore>
 		);
 		expect(container).toMatchSnapshot();
