bd8f5c25937037e40ddc0d3731a885cad6e9ed59
adamhinton <adam.d.hinton@gmail.com>
2025-05-19 14:21:34 -0400

    WIP fixing capitalizaiton issues

--- supabase/migrations/20250519180213_create_trips_table.sql
+++ supabase/migrations/20250519180213_create_trips_table.sql
@@ -9,8 +9,8 @@ CREATE TABLE trips (
     -- Stuff not used in cost calculation
     id SERIAL PRIMARY KEY,
     "userID" uuid NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (userID) REFERENCES auth.users(id) ON DELETE CASCADE,
-    tripsOrder INT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY ("userID") REFERENCES auth.users(id) ON DELETE CASCADE,
+    "tripsOrder" INT NOT NULL,
 
     -- These help the user classify and understand
     name VARCHAR(100) NOT NULL,
@@ -19,8 +19,8 @@ CREATE TABLE trips (
     notes VARCHAR(500),
 
     -- Now trip cost calculation stuff
-    tripType VARCHAR(20) NOT NULL CHECK(tripType IN ('SHORT_DISTANCE', 'LONG_DISTANCE')),
-    roundTripDrivingDistanceMiles DECIMAL(7, 1),
+    "tripType" VARCHAR(20) NOT NULL CHECK("tripType" IN ('SHORT_DISTANCE', 'LONG_DISTANCE')),
+    "roundTripDrivingDistanceMiles" DECIMAL(7, 1),
     
     -- Can have multiple TripOptions - one to many relationship - that relationship is modeled in TripOption's foreign key
 
