9b7497a34a4ae724dbe97a9f27b57b69d5a66912
adamhinton <adam.d.hinton@gmail.com>
2025-05-19 14:34:01 -0400

    Seed Trips and TripOptions in to dev db for testing

--- supabase/migrations/20250519180213_create_trips_table.sql
+++ supabase/migrations/20250519180213_create_trips_table.sql
@@ -3,24 +3,31 @@
 -- It corresponds to TripSchema
 -- ___________________________________________________________
 
-DROP TABLE IF EXISTS trips;
+DROP TABLE IF EXISTS trips cascade;
 
 CREATE TABLE trips (
     -- Stuff not used in cost calculation
     id SERIAL PRIMARY KEY,
-    userID uuid NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (userID) REFERENCES auth.users(id) ON DELETE CASCADE,
+    "userID" uuid NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY ("userID") REFERENCES auth.users(id) ON DELETE CASCADE,
+    "tripsOrder" INT NOT NULL,
 
     -- These help the user classify and understand
     name VARCHAR(100) NOT NULL,
     destination VARCHAR(100) NOT NULL,
     origin VARCHAR(100),
     notes VARCHAR(500),
+    "departureDate" DATE,
+    "returnDate" DATE,
+    
 
     -- Now trip cost calculation stuff
-    tripType VARCHAR(20) NOT NULL CHECK(tripType IN ('SHORT_DISTANCE', 'LONG_DISTANCE')),
-    roundTripDrivingDistanceMiles DECIMAL(7, 1),
-    
+    -- These are part of LongDistanceTripSchema
+    "tripType" VARCHAR(20) NOT NULL CHECK("tripType" IN ('SHORT_DISTANCE', 'LONG_DISTANCE')),
+    "roundTripDrivingDistanceMiles" DECIMAL(7, 1),
+    "localDrivingDistanceMiles" DECIMAL(7, 1),
+
+
     -- Can have multiple TripOptions - one to many relationship - that relationship is modeled in TripOption's foreign key
 
     "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
