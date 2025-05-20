7a8be662d531a57071e9a09eb2a993e4708667c1
adamhinton <adam.d.hinton@gmail.com>
2025-05-20 13:38:21 -0400

    Write Trips supabase db queries

--- src/app/utils/server/queries/trips/tripsDBUtils.ts
+++ src/app/utils/server/queries/trips/tripsDBUtils.ts
@@ -1,4 +1,27 @@
+"use server";
+
+import { QueryData } from "@supabase/supabase-js";
+import { createClientSSROnly } from "../../supabase/server";
+
 // _______________________________________________________________
 // This is (obivously) utils for interacting with the DB w.r.t Trips
 // Not sure yet if I'll put TripOptions stuff in here or another file; probably another file
 // _______________________________________________________________
+
+/**Returns a supabase sql-type statement to get all of a user's Trips
+ * This doesn't get a Trip's TripOptions; another function (to be written) will get those and combine it with this Trip
+ */
+export const getTripsByUserIdQuery = async (userId: string) => {
+	const supabase = await createClientSSROnly();
+	return supabase.from("trips").select("*").eq("userid", userId);
+};
+
+/**Returns a supabase sql-type statement to get a single Trip by its id
+ * Doesn't get a Trip's TripOptions; another function will get those and combine it with this where neeed
+ */
+export const getSingleTripByIdQuery = async (tripId: number) => {
+	const supabase = await createClientSSROnly();
+	return supabase.from("trips").select("*").eq("id", tripId);
+};
+
+export type TripFromDB = Promise<QueryData<typeof getSingleTripByIdQuery>>;
