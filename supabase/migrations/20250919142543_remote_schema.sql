8231af636d264b8dcf8227cf1b169ce87f3ae18f
adamhinton <adam.d.hinton@gmail.com>
2025-09-19 11:19:19 -0400

    Update to new supabase project

---
+++ supabase/migrations/20250919142543_remote_schema.sql
@@ -0,0 +1,503 @@
+revoke delete on table "public"."electricVehicleData" from "anon";
+
+revoke insert on table "public"."electricVehicleData" from "anon";
+
+revoke references on table "public"."electricVehicleData" from "anon";
+
+revoke select on table "public"."electricVehicleData" from "anon";
+
+revoke trigger on table "public"."electricVehicleData" from "anon";
+
+revoke truncate on table "public"."electricVehicleData" from "anon";
+
+revoke update on table "public"."electricVehicleData" from "anon";
+
+revoke delete on table "public"."electricVehicleData" from "authenticated";
+
+revoke insert on table "public"."electricVehicleData" from "authenticated";
+
+revoke references on table "public"."electricVehicleData" from "authenticated";
+
+revoke select on table "public"."electricVehicleData" from "authenticated";
+
+revoke trigger on table "public"."electricVehicleData" from "authenticated";
+
+revoke truncate on table "public"."electricVehicleData" from "authenticated";
+
+revoke update on table "public"."electricVehicleData" from "authenticated";
+
+revoke delete on table "public"."electricVehicleData" from "service_role";
+
+revoke insert on table "public"."electricVehicleData" from "service_role";
+
+revoke references on table "public"."electricVehicleData" from "service_role";
+
+revoke select on table "public"."electricVehicleData" from "service_role";
+
+revoke trigger on table "public"."electricVehicleData" from "service_role";
+
+revoke truncate on table "public"."electricVehicleData" from "service_role";
+
+revoke update on table "public"."electricVehicleData" from "service_role";
+
+revoke delete on table "public"."fixedCosts" from "anon";
+
+revoke insert on table "public"."fixedCosts" from "anon";
+
+revoke references on table "public"."fixedCosts" from "anon";
+
+revoke select on table "public"."fixedCosts" from "anon";
+
+revoke trigger on table "public"."fixedCosts" from "anon";
+
+revoke truncate on table "public"."fixedCosts" from "anon";
+
+revoke update on table "public"."fixedCosts" from "anon";
+
+revoke delete on table "public"."fixedCosts" from "authenticated";
+
+revoke insert on table "public"."fixedCosts" from "authenticated";
+
+revoke references on table "public"."fixedCosts" from "authenticated";
+
+revoke select on table "public"."fixedCosts" from "authenticated";
+
+revoke trigger on table "public"."fixedCosts" from "authenticated";
+
+revoke truncate on table "public"."fixedCosts" from "authenticated";
+
+revoke update on table "public"."fixedCosts" from "authenticated";
+
+revoke delete on table "public"."fixedCosts" from "service_role";
+
+revoke insert on table "public"."fixedCosts" from "service_role";
+
+revoke references on table "public"."fixedCosts" from "service_role";
+
+revoke select on table "public"."fixedCosts" from "service_role";
+
+revoke trigger on table "public"."fixedCosts" from "service_role";
+
+revoke truncate on table "public"."fixedCosts" from "service_role";
+
+revoke update on table "public"."fixedCosts" from "service_role";
+
+revoke delete on table "public"."gasVehicleData" from "anon";
+
+revoke insert on table "public"."gasVehicleData" from "anon";
+
+revoke references on table "public"."gasVehicleData" from "anon";
+
+revoke select on table "public"."gasVehicleData" from "anon";
+
+revoke trigger on table "public"."gasVehicleData" from "anon";
+
+revoke truncate on table "public"."gasVehicleData" from "anon";
+
+revoke update on table "public"."gasVehicleData" from "anon";
+
+revoke delete on table "public"."gasVehicleData" from "authenticated";
+
+revoke insert on table "public"."gasVehicleData" from "authenticated";
+
+revoke references on table "public"."gasVehicleData" from "authenticated";
+
+revoke select on table "public"."gasVehicleData" from "authenticated";
+
+revoke trigger on table "public"."gasVehicleData" from "authenticated";
+
+revoke truncate on table "public"."gasVehicleData" from "authenticated";
+
+revoke update on table "public"."gasVehicleData" from "authenticated";
+
+revoke delete on table "public"."gasVehicleData" from "service_role";
+
+revoke insert on table "public"."gasVehicleData" from "service_role";
+
+revoke references on table "public"."gasVehicleData" from "service_role";
+
+revoke select on table "public"."gasVehicleData" from "service_role";
+
+revoke trigger on table "public"."gasVehicleData" from "service_role";
+
+revoke truncate on table "public"."gasVehicleData" from "service_role";
+
+revoke update on table "public"."gasVehicleData" from "service_role";
+
+revoke delete on table "public"."hybridVehicleData" from "anon";
+
+revoke insert on table "public"."hybridVehicleData" from "anon";
+
+revoke references on table "public"."hybridVehicleData" from "anon";
+
+revoke select on table "public"."hybridVehicleData" from "anon";
+
+revoke trigger on table "public"."hybridVehicleData" from "anon";
+
+revoke truncate on table "public"."hybridVehicleData" from "anon";
+
+revoke update on table "public"."hybridVehicleData" from "anon";
+
+revoke delete on table "public"."hybridVehicleData" from "authenticated";
+
+revoke insert on table "public"."hybridVehicleData" from "authenticated";
+
+revoke references on table "public"."hybridVehicleData" from "authenticated";
+
+revoke select on table "public"."hybridVehicleData" from "authenticated";
+
+revoke trigger on table "public"."hybridVehicleData" from "authenticated";
+
+revoke truncate on table "public"."hybridVehicleData" from "authenticated";
+
+revoke update on table "public"."hybridVehicleData" from "authenticated";
+
+revoke delete on table "public"."hybridVehicleData" from "service_role";
+
+revoke insert on table "public"."hybridVehicleData" from "service_role";
+
+revoke references on table "public"."hybridVehicleData" from "service_role";
+
+revoke select on table "public"."hybridVehicleData" from "service_role";
+
+revoke trigger on table "public"."hybridVehicleData" from "service_role";
+
+revoke truncate on table "public"."hybridVehicleData" from "service_role";
+
+revoke update on table "public"."hybridVehicleData" from "service_role";
+
+revoke delete on table "public"."purchaseAndSales" from "anon";
+
+revoke insert on table "public"."purchaseAndSales" from "anon";
+
+revoke references on table "public"."purchaseAndSales" from "anon";
+
+revoke select on table "public"."purchaseAndSales" from "anon";
+
+revoke trigger on table "public"."purchaseAndSales" from "anon";
+
+revoke truncate on table "public"."purchaseAndSales" from "anon";
+
+revoke update on table "public"."purchaseAndSales" from "anon";
+
+revoke delete on table "public"."purchaseAndSales" from "authenticated";
+
+revoke insert on table "public"."purchaseAndSales" from "authenticated";
+
+revoke references on table "public"."purchaseAndSales" from "authenticated";
+
+revoke select on table "public"."purchaseAndSales" from "authenticated";
+
+revoke trigger on table "public"."purchaseAndSales" from "authenticated";
+
+revoke truncate on table "public"."purchaseAndSales" from "authenticated";
+
+revoke update on table "public"."purchaseAndSales" from "authenticated";
+
+revoke delete on table "public"."purchaseAndSales" from "service_role";
+
+revoke insert on table "public"."purchaseAndSales" from "service_role";
+
+revoke references on table "public"."purchaseAndSales" from "service_role";
+
+revoke select on table "public"."purchaseAndSales" from "service_role";
+
+revoke trigger on table "public"."purchaseAndSales" from "service_role";
+
+revoke truncate on table "public"."purchaseAndSales" from "service_role";
+
+revoke update on table "public"."purchaseAndSales" from "service_role";
+
+revoke delete on table "public"."trip_options" from "anon";
+
+revoke insert on table "public"."trip_options" from "anon";
+
+revoke references on table "public"."trip_options" from "anon";
+
+revoke select on table "public"."trip_options" from "anon";
+
+revoke trigger on table "public"."trip_options" from "anon";
+
+revoke truncate on table "public"."trip_options" from "anon";
+
+revoke update on table "public"."trip_options" from "anon";
+
+revoke delete on table "public"."trip_options" from "authenticated";
+
+revoke insert on table "public"."trip_options" from "authenticated";
+
+revoke references on table "public"."trip_options" from "authenticated";
+
+revoke select on table "public"."trip_options" from "authenticated";
+
+revoke trigger on table "public"."trip_options" from "authenticated";
+
+revoke truncate on table "public"."trip_options" from "authenticated";
+
+revoke update on table "public"."trip_options" from "authenticated";
+
+revoke delete on table "public"."trip_options" from "service_role";
+
+revoke insert on table "public"."trip_options" from "service_role";
+
+revoke references on table "public"."trip_options" from "service_role";
+
+revoke select on table "public"."trip_options" from "service_role";
+
+revoke trigger on table "public"."trip_options" from "service_role";
+
+revoke truncate on table "public"."trip_options" from "service_role";
+
+revoke update on table "public"."trip_options" from "service_role";
+
+revoke delete on table "public"."trips" from "anon";
+
+revoke insert on table "public"."trips" from "anon";
+
+revoke references on table "public"."trips" from "anon";
+
+revoke select on table "public"."trips" from "anon";
+
+revoke trigger on table "public"."trips" from "anon";
+
+revoke truncate on table "public"."trips" from "anon";
+
+revoke update on table "public"."trips" from "anon";
+
+revoke delete on table "public"."trips" from "authenticated";
+
+revoke insert on table "public"."trips" from "authenticated";
+
+revoke references on table "public"."trips" from "authenticated";
+
+revoke select on table "public"."trips" from "authenticated";
+
+revoke trigger on table "public"."trips" from "authenticated";
+
+revoke truncate on table "public"."trips" from "authenticated";
+
+revoke update on table "public"."trips" from "authenticated";
+
+revoke delete on table "public"."trips" from "service_role";
+
+revoke insert on table "public"."trips" from "service_role";
+
+revoke references on table "public"."trips" from "service_role";
+
+revoke select on table "public"."trips" from "service_role";
+
+revoke trigger on table "public"."trips" from "service_role";
+
+revoke truncate on table "public"."trips" from "service_role";
+
+revoke update on table "public"."trips" from "service_role";
+
+revoke delete on table "public"."usage" from "anon";
+
+revoke insert on table "public"."usage" from "anon";
+
+revoke references on table "public"."usage" from "anon";
+
+revoke select on table "public"."usage" from "anon";
+
+revoke trigger on table "public"."usage" from "anon";
+
+revoke truncate on table "public"."usage" from "anon";
+
+revoke update on table "public"."usage" from "anon";
+
+revoke delete on table "public"."usage" from "authenticated";
+
+revoke insert on table "public"."usage" from "authenticated";
+
+revoke references on table "public"."usage" from "authenticated";
+
+revoke select on table "public"."usage" from "authenticated";
+
+revoke trigger on table "public"."usage" from "authenticated";
+
+revoke truncate on table "public"."usage" from "authenticated";
+
+revoke update on table "public"."usage" from "authenticated";
+
+revoke delete on table "public"."usage" from "service_role";
+
+revoke insert on table "public"."usage" from "service_role";
+
+revoke references on table "public"."usage" from "service_role";
+
+revoke select on table "public"."usage" from "service_role";
+
+revoke trigger on table "public"."usage" from "service_role";
+
+revoke truncate on table "public"."usage" from "service_role";
+
+revoke update on table "public"."usage" from "service_role";
+
+revoke delete on table "public"."variableCosts" from "anon";
+
+revoke insert on table "public"."variableCosts" from "anon";
+
+revoke references on table "public"."variableCosts" from "anon";
+
+revoke select on table "public"."variableCosts" from "anon";
+
+revoke trigger on table "public"."variableCosts" from "anon";
+
+revoke truncate on table "public"."variableCosts" from "anon";
+
+revoke update on table "public"."variableCosts" from "anon";
+
+revoke delete on table "public"."variableCosts" from "authenticated";
+
+revoke insert on table "public"."variableCosts" from "authenticated";
+
+revoke references on table "public"."variableCosts" from "authenticated";
+
+revoke select on table "public"."variableCosts" from "authenticated";
+
+revoke trigger on table "public"."variableCosts" from "authenticated";
+
+revoke truncate on table "public"."variableCosts" from "authenticated";
+
+revoke update on table "public"."variableCosts" from "authenticated";
+
+revoke delete on table "public"."variableCosts" from "service_role";
+
+revoke insert on table "public"."variableCosts" from "service_role";
+
+revoke references on table "public"."variableCosts" from "service_role";
+
+revoke select on table "public"."variableCosts" from "service_role";
+
+revoke trigger on table "public"."variableCosts" from "service_role";
+
+revoke truncate on table "public"."variableCosts" from "service_role";
+
+revoke update on table "public"."variableCosts" from "service_role";
+
+revoke delete on table "public"."vehicleData" from "anon";
+
+revoke insert on table "public"."vehicleData" from "anon";
+
+revoke references on table "public"."vehicleData" from "anon";
+
+revoke select on table "public"."vehicleData" from "anon";
+
+revoke trigger on table "public"."vehicleData" from "anon";
+
+revoke truncate on table "public"."vehicleData" from "anon";
+
+revoke update on table "public"."vehicleData" from "anon";
+
+revoke delete on table "public"."vehicleData" from "authenticated";
+
+revoke insert on table "public"."vehicleData" from "authenticated";
+
+revoke references on table "public"."vehicleData" from "authenticated";
+
+revoke select on table "public"."vehicleData" from "authenticated";
+
+revoke trigger on table "public"."vehicleData" from "authenticated";
+
+revoke truncate on table "public"."vehicleData" from "authenticated";
+
+revoke update on table "public"."vehicleData" from "authenticated";
+
+revoke delete on table "public"."vehicleData" from "service_role";
+
+revoke insert on table "public"."vehicleData" from "service_role";
+
+revoke references on table "public"."vehicleData" from "service_role";
+
+revoke select on table "public"."vehicleData" from "service_role";
+
+revoke trigger on table "public"."vehicleData" from "service_role";
+
+revoke truncate on table "public"."vehicleData" from "service_role";
+
+revoke update on table "public"."vehicleData" from "service_role";
+
+revoke delete on table "public"."vehicles" from "anon";
+
+revoke insert on table "public"."vehicles" from "anon";
+
+revoke references on table "public"."vehicles" from "anon";
+
+revoke select on table "public"."vehicles" from "anon";
+
+revoke trigger on table "public"."vehicles" from "anon";
+
+revoke truncate on table "public"."vehicles" from "anon";
+
+revoke update on table "public"."vehicles" from "anon";
+
+revoke delete on table "public"."vehicles" from "authenticated";
+
+revoke insert on table "public"."vehicles" from "authenticated";
+
+revoke references on table "public"."vehicles" from "authenticated";
+
+revoke select on table "public"."vehicles" from "authenticated";
+
+revoke trigger on table "public"."vehicles" from "authenticated";
+
+revoke truncate on table "public"."vehicles" from "authenticated";
+
+revoke update on table "public"."vehicles" from "authenticated";
+
+revoke delete on table "public"."vehicles" from "service_role";
+
+revoke insert on table "public"."vehicles" from "service_role";
+
+revoke references on table "public"."vehicles" from "service_role";
+
+revoke select on table "public"."vehicles" from "service_role";
+
+revoke trigger on table "public"."vehicles" from "service_role";
+
+revoke truncate on table "public"."vehicles" from "service_role";
+
+revoke update on table "public"."vehicles" from "service_role";
+
+revoke delete on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke insert on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke references on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke select on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke trigger on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke truncate on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke update on table "public"."yearlyMaintenanceCosts" from "anon";
+
+revoke delete on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke insert on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke references on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke select on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke trigger on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke truncate on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke update on table "public"."yearlyMaintenanceCosts" from "authenticated";
+
+revoke delete on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke insert on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke references on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke select on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke trigger on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke truncate on table "public"."yearlyMaintenanceCosts" from "service_role";
+
+revoke update on table "public"."yearlyMaintenanceCosts" from "service_role";
