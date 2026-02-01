58180aba28d3b3fae20d270b12417337fcc79c49
adamhinton <adam.d.hinton@gmail.com>
2026-02-01 18:53:14 -0500

    Better notes

--- src/app/utils/server/supabase/client.ts
+++ src/app/utils/server/supabase/client.ts
@@ -7,10 +7,12 @@ import { createBrowserClient } from "@supabase/ssr";
 /**Supabase names this createClient() in their docs, I renamed it for clarity
  *
  * And I definitely won't regret that later when copying from their docs
+ *
+ * This runs on the CLIENT ONLY; if you need supabase client for server-side code use createClientSSROnly()
  */
 export function createClientCSROnly() {
 	return createBrowserClient(
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 	);
 }
